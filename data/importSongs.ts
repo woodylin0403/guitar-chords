8e90eee8aa5fb277fe9e0777fd615fb6d0e528d4
woodylin0403 <coolcrow0403@gmail.com>
2026-03-09 14:55:13 +0800

    song16

--- data/importSongs.ts
+++ data/importSongs.ts
@@ -1,51 +1,58 @@
 export const importSongs = [
   {
-    title: "1. 平安之王",
+    title: "8. 榮耀君王我們尊崇你",
+    originalKey: "D",
+    timeSignature: "4/4",
+    editor: "烏鴉Lin",
+    content: "[D]榮耀君[Em]王 我們尊崇[F#m]你 [Bm]你作[G]王直到 [C#7]永\n[F#m]遠 我們[A7]虔誠[D]獻上 全[G]心來敬拜讚[Gm]美\n[D]來彰顯你聖[EmA7]潔的名 [D]全[G]能的[D]主宰 榮[Bm]耀\n中掌[F#m]權 永遠君[Em]王 掌權到永[A]遠 權能的\n[Bm]話語 賜給我[F#m]自由 你愛真寶[Em]貴 我主[A7]我\n[D]神[G][D]"
+  },
+  {
+    title: "9. 你慈愛何等寶貴",
+    originalKey: "D",
+    timeSignature: "4/4",
+    editor: "烏鴉Lin",
+    content: "[D]哦 主啊 [Em7]你慈[A7]愛是何[D]等的寶[Em]貴 我們投靠\n[A]在你翅膀的[D]蔭下[Em7][A]主[D]啊 何等豐[Em7]盛 當\n[A7]我們與你[D]同坐席 [Em7]飲於[A]你樂[D]河的活[D7]水\n副 [G]哦主 你是[A]生命本[F#m]源[Bm7] 在你[Em]光中 必得[A7]\n見[D]光[D7] [G]哦主 你是[A]生命本[F#m]源[Bm7] 在你[Em]光中\n必得[A7]見[D]光[G]"
+  },
+  {
+    title: "10. 手之歌",
+    originalKey: "D",
+    timeSignature: "4/4",
+    editor: "烏鴉Lin",
+    content: "[D]手牽著[G]手來 [D]唱哈利路[G]亞 唱哈利路[D]亞\n[A]手牽著[1.A7]手 [2.D]手\n[G]哈利路[D]亞 哈利路[A]亞 哈利路[D]亞 手牽手\n[G]哈利路[D]亞 哈利路[A]亞 哈利路[G]亞 手牽手\nP.S 哈利路亞可改唱高舉雙手"
+  },
+  {
+    title: "11. 當我進入祂的門",
+    originalKey: "D",
+    timeSignature: "4/4",
+    editor: "烏鴉Lin",
+    content: "[D]當我進入祂的[G]門 我的心要[D]稱謝祂 當我\n[G]進入祂的院[A7]讚美主 [D]因為這[G]是耶和華所[D]定\n[D]的日子 我要[Em]歡欣[A7]因祂使我[D]喜樂\n副 [G]祂使我[D]喜樂 祂使我[Em]喜樂 [A7]我要歡欣因祂\n[D]使我[A7]喜樂 [D]祂使我[G]喜樂 祂使我[D]喜樂\n[Em]我要歡欣[A7]因祂使我[D]喜樂[G][D]"
+  },
+  {
+    title: "12. 軟化我心",
     originalKey: "D",
     timeSignature: "3/4",
     editor: "烏鴉Lin",
-    content: "[D]平安之王耶[F#m7G]穌 [Em]慈悲的[Em7]主上[A]帝 [A7]耶[F#m]穌\n[F#m]永遠的天[Bm]父 [G]你是[A7]萬王之[D]王[G][D]"
+    content: "[D]軟化我[Em]心 主[Em]軟化[A]我心 [D]求[D]使我不再[Em]\n[A]漠不關心[D] 能[F#m]體會[D]你憐憫 與[G]你[A]\n[F#m7]同憂[Bm7]傷 求[D]軟化[F#m]我心 [Bm]哦主求軟化我[D]心"
   },
   {
-    title: "2. 主啊，我深願",
+    title: "13. 歡樂像雨點",
     originalKey: "D",
     timeSignature: "4/4",
     editor: "烏鴉Lin",
-    content: "[D]主啊我深願 [F#m7G]親近你 更親近[D]你[A]\n1.[G]悅納我[A]心 使 [F#mBmEm7]我親近 更親近[D]你\n2.[G]導我前[A]行 使 [F#mBmEm7]我親近 更親近[D]你\n3.[G]吸引我[A]靈深 處[F#mBmEm7]親近 更親近[D]你\n4.[G]主 正呼[A]召 我[F#mBmEm7]親近 更親近[D]你\n[C]主啊我深願更[G]親近 [D]主啊我深[C]願[G]\n更親[D]近 [C]主啊我深願更[G]親近 [A7]親[Em7]\n近[Em7]你 更親近[D]你 親近你 更親[Em7]近\n[D]你 親近[Em7]你 更親近[D]你"
+    content: "1.[D]我見雨[G]點滴在[A7]窗前 [D]歡樂像[G]雨點 [A7D]歡笑來[A7D]\n2.我見白雲飄過山岡 歡樂像白雲 有時燦\n3.人生雖然遇到風浪 歡樂受考驗 但有耶\n1.[G]臨痛苦[D]去 快樂又[A7]重新出[D]現 [G]歡樂像雨[A7D]點[A7D]\n2.爛有時暗 雲上太陽總不變 歡樂像白雲\n3.穌在船上 雖有風暴無驚險 歡樂受考驗"
   },
   {
-    title: "3. 神愛你",
+    title: "14. 為什麼",
     originalKey: "D",
     timeSignature: "4/4",
     editor: "烏鴉Lin",
-    content: "1.[D]世上最甜蜜的故事 [A7]神愛你 最光彩美[Em7]\n2.當勇氣消失壓力重 神愛你 你當記住\n1.[A7]麗的圖畫 [D]神愛你 [Am7]當你前途如黑夜[D7]\n2.在試煉中 神愛你 希望好像已破滅\n1.[G]主保守你不[Gm]傾跌 [D]祂必緊握你的手 神[Em7]\n2.你覺得非常孤單 向上仰望見曙光 神\n1.[D]愛你\n2.愛你"
+    content: "[D]為什麼美[Bm]好的時光 [Em]總是那麼[A]的短[D]暫 為\n[Bm]什麼親[Em]愛的朋友 [A]總是要[D]分手 春[Bm]去秋來\n[Em]花開花[A]謝 告[D]訴我 [Bm]為什麼[Em]好景[A]不常[D]在"
   },
   {
-    title: "4. 榮耀神羔羊",
+    title: "15. 每日要禱告主",
     originalKey: "D",
     timeSignature: "4/4",
     editor: "烏鴉Lin",
-    content: "[D]榮耀 [A]榮耀 [G]榮耀神羔[Em7]羊 [A]榮耀[D]\n[A]榮耀 [Em7]榮耀神羔[A]羊 [D]惟有祂是榮[G]\n[A]耀 尊貴[F#m7]配得讚[Bm7]美 [Em7]在寶座上的[A]\n[C]真[D]神[C]羔[D]羊 [G]來敬拜[A]祂高聲齊來頌[F#m7]\n[Bm7]揚在寶[Em7]座上[A]的羔羊[D]"
-  },
-  {
-    title: "5. 萬國都要來讚美主",
-    originalKey: "D",
-    timeSignature: "4/4",
-    editor: "烏鴉Lin",
-    content: "[D]萬國都要來讚美主 [Bm]萬民都要來讚美[D]\n[F#m]主 敬拜耶和[G]華唱[A7]詩來向祂歡呼 [D]讚美[Bm]\n[G]耶和[A7]華萬民都[D]快樂歡呼 [Bm]敬拜讚美耶\n[F#m]和華 [Bm]凡有氣息都要讚美祂 [F#m]敬拜\n[G]耶和華 [D]讚美耶和[Bm]華 我們活著是為[G]\n[A7]要敬拜讚美祂 [D]我們活著是[Bm]為[G]要敬拜[A7]\n[D]讚美祂"
-  },
-  {
-    title: "6. 超越過一切",
-    originalKey: "D",
-    timeSignature: "4/4",
-    editor: "烏鴉Lin",
-    content: "[D]超越過一切 [A/E]超越過一[F#m7]切 我愛[Bm7]\n[Em7]你耶[G/A]穌 超越過一[D]切[Bm7]\n1.[Em7]超越過一[D]切 [A/E]超越過一[F#m7]切 我愛[Bm7]\n2.超越全世界 超越我生命 我愛\n[Em7]你耶[G/A]穌 超越過一[D]切 今生今[F]世\n[D]無人能相[F]比 也無人奪[D]去你愛 [Bm]有\n[E]一日我與你面[Bm]對面 我要親口對你說[A]"
-  },
-  {
-    title: "7. 神啊 我願順從你",
-    originalKey: "D",
-    timeSignature: "4/4",
-    editor: "烏鴉Lin",
-    content: "[D]神啊 [F#m]我願順從[G]你 [A7]我要聽從你的話[D] (二次)\n[G]悖逆的心 [D]我願除[F#7]掉 [Bm]頑梗的意[G]念 我願[A7]\n[D]放下 [G]悖逆的心 [D]我願除[G]掉[A7] [Bm]頑梗的意念[Em][Am]\n[D]我願放下"
+    content: "[D]每日要禱告[A]主 你要記得每[D]天要禱告 要有\n[G]活潑的[D]信仰 靈[A]性必須不[D]斷餵[G]養[D]"
   }
 ];
