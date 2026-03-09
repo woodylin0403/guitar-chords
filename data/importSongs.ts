5e359c2ed7772a69dd621942320698b72b86bba8
woodylin0403 <coolcrow0403@gmail.com>
2026-03-09 12:32:25 +0800

    song14

--- data/importSongs.ts
+++ data/importSongs.ts
@@ -1,142 +1,121 @@
 export const importSongs = [
     {
-      title: "1. 來讚美祂",
+      title: "21. 祂感動我",
       originalKey: "F",
       timeSignature: "3/4",
       editor: "烏鴉Lin",
-      content: "[F]來讚美[Dm]來敬拜[Bb]來尊崇[Gm7]主名\n[G7]從今時[C]到永遠祂總不改變\n[F]來讚美[Dm]我救主[Bb]被殺的羔羊\n[Gm7]來讚美[F]來敬拜[Gm]主耶[C]穌聖[F]名"
+      content: "[F] [C7] [F] [C7]\n(一) 苦難和重擔壓制我 罪惡羞辱淹滅\n(二) 自從蒙恩見主榮面 自從罪得主赦\n[F] [Bb] [F] [C7]\n(一) 我 但耶穌親手撫慰我 使重擔和\n(二) 冤 我要歌頌主到永遠 高聲讚美\n[F]\n(一) 羞辱離開我\n(二) 主祂永不變\n[C7] [F] [B6] [F]\n副歌：祂愛我 哦 祂愛我 祂賜下喜樂充\n[C7] [F] [C7] [F] [Bb] [F]\n滿我 不再猶豫 不再疑惑 祂愛我\n[F] [C7] [F]\n使我 完全"
     },
     {
-      title: "2. 榮耀歸祂",
+      title: "22. 活出生命的色彩",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F] [Dm] [Bb] [C]\n1.走出心靈的陰霾 活出生命的色彩\n[Am] [Dm] [Bb] [G/B] [C]\n別讓青春再期待 改造每一個現在\n[F] [Dm] [Bb] [C]\n2.走出心靈的陰霾 活出生命的色彩\n[Am] [Dm] [G] [C7F]\n別讓青春再期待 改造每一個現在\n[C] [F]\n(副)不要 不要 不要再自怨自艾 停止\n[C] [F] [Bb] [C]\n停止 停止那消極無奈 千萬人中\n[Am] [Dm] [Bb] [G/B] [C]\n你獨一無二 堅強的活出 生命的色彩\n[F] [Bb] [C]\n走出心靈的陰霾 活出生命的色彩\n[Am] [Dm] [Bb] [C] [F]\n展開向上的翅膀迎向希望的未來 (二次)"
+    },
+    {
+      title: "23. 全能醫治者",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F] [Am7] [Gm7] [Bb/C] [C7BbF]\n1. 主耶和華 我醫治者 他是全能 醫治者\n2. 你是我神 來醫治我 你是全能 醫治者\n[Dm] [Gm] [C7]\n1. 他已發命來 醫治我疾病 主耶和華\n2. 你已發命來 醫治我疾病 你是我神\n[Gm] [F]\n1. 我醫治者\n2. 來醫治我"
+    },
+    {
+      title: "24. 再給我一次機會",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F] [Dm] [Bb] [A]\n再給我一個機會 不要使我失去今天\n[Bb] [F] [Gm7] [C7]\n再給我一個機會 不要使我悔恨連連 (反覆)\n[Gm7] [F]\n(第二次) 不要使我悔恨連連\n[Fm] [Bb] [Fm] [C7]\n主啊 赦免我 赦免我無知的過去\n[Db] [Ab] [Db] [C7]\n主啊 接受我 接受我破碎的心靈 (反覆)"
+    },
+    {
+      title: "25. 獻上活祭",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[C7F] [Dm] [BbC] [F]\n1. 我心切切渴慕你主 我要緊緊跟\n2. 我心深深向你敬拜 我靈渴慕你\n[Am] [Bb] [C] [F] [Dm]\n1. 隨你 你恩典與慈愛永無止息\n2. 榮光 我完全相信你寶貴應許\n[Bb] [Dm] [F] [Bb] [C]\n1. 你信實直到永遠 我王我的神\n2. 我在你懷中安息 我要向世人\n[F] [Dm] [Bb] [Dm] [F]\n1. 因你奇妙聖名 獻上我的讚美\n2. 歌唱你的恩典 直到我見你面\n[F] [Am] [Bb/Gm] [C7] [F]\n耶穌 主耶穌 求用 我一生 (二次)"
+    },
+    {
+      title: "26. 湧出活水",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F] [Bb] [F] [F]\n生命的活水從我湧出來 瘸腿者行路\n[C7] [F] [Bb] [F]\n瞎眼看得見 打開監獄門釋放被擄者\n[F] [Bb] [F] [F]\n生命的活水從我湧出來 湧出活水\n[Bb] [F] [F] [G] [C7]\n從我靈深處 湧出活水使我得痊癒\n[F] [Bb] [F] [Bb] [C7] [F]\n湧出活水並賜給我 那豐盛的生命"
+    },
+    {
+      title: "27. 活水湧流",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F] [F]\n在我的心中 活水湧流出來 喔湧流出來\n[Gm] [C] [F] [F]\n滿溢我身 喔湧流出來 不斷的湧流出來\n[F] [C] [C7] [F] [Bb] [F]\n去釋放衆人 人心得釋放"
+    },
+    {
+      title: "28. 我們又在一起",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F] [Bb] [Gm7] [C]\n我們又在一起 來讚美主 我們又在一起\n[F] [Bb] [F] [F7Bb]\n同心合意 美好事必定要成就 美好事\n[Gm7] [C7] [F]\n已顯明 我們又在一起 來讚美主"
+    },
+    {
+      title: "29. 我願成為主的喜樂",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F] [Bb] [Gm7]\n我一生渴慕成為主的喜樂 將我的心\n[C7] [Gm7F] [Bb]\n獻在主的面前 塑造我成為聖潔的器皿\n[Gm7] [Bb] [Gm7BbC] [F] [C]\n如同精金彰顯主的榮耀 哦主 我愛你\n[Bb] [F] [Bb] [Gm7] [C] [Bb] [C]\n敬拜你 我願成為主你的喜樂 哦主\n[F] [C] [BbC] [Dm] [Gm7] [C7] [F]\n我愛你 敬拜你 我願成為主你的喜樂"
+    },
+    {
+      title: "30. 倚靠祂就得幫助",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[C7] [F] [Bb] [Gm] [C7] [F] [C7]\n哦 啦 ... 主聽我禱告 聽我的懇求\n[F] [Bb] [C7] [F]\n哦 啦 ... 我倚靠祂就得幫助\n[Bb] [Bd1m] [F/C] [C7]\n我心中歡樂 我要讚美他 我要讚美他\n[F] [Bb] [Bd1m] [Am] [Dm]\n我要讚美他 我心中歡樂 我要讚美他\n[F/C] [C] [F]\n我倚靠他就得幫助"
+    },
+    {
+      title: "31. 主為我預備道路",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F] [C7] [F]\n我知救主為我預備道路 我知救主為\n[C7] [F] [Bb] [F]\n我預備道路 仰望主懇切祈求 雖黑夜\n[F] [C7] [F]\n也變白晝 我知救主為我 預備道路"
+    },
+    {
+      title: "32. 你要微笑",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F] [Gm] [C7]\n要微笑 無論你是否憂傷 要微笑\n[Gm] [C7] [F] [Bb] [Bbm] [F]\n因 主就在你身旁 微笑使你一天天\n[E] [D7] [G7] [F] [C7]\n更明亮 微笑使你重擔更覺輕省 因\n[F] [Gm] [C7]\n今天這是個更有價值一天 讚美主\n[G] [Gm] [C] [F] [Bb] [Bbm] [F]\n感恩充滿你的心 那明朗而欣喜的\n[D7] [Gm7] [C7] [F]\n微笑使我生命更富意義 (反覆)\n[Bb] [Bb] [Bmb] [F]\n使我生命更富 意義"
+    },
+    {
+      title: "33. 我已找到快樂途徑",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F] [Am] [Gm] [C7] [F]\n(一) 人類每天切心尋求日光下的新事\n(二) 我已找到無價珍珠永遠長存美好\n[F] [Dm] [G] [C7]\n(一) 但我已找到生命路正確愉快完美\n(二) 藉著救主偉大犧牲我得著這珍貴\n[F] [F]\n副歌：我已找到快樂的途徑 我已到歡\n[Gm] [C7] [Bb]\n欣之路 我已找到如何除憂傷 這\n[F] [F]\n是完美純真的喜樂 我已找到賜福\n[F] [Bb]\n的泉源 我已找到生命之道 當我\n[Bb] [F] [Dm] [Bb] [C7]\n找到耶穌為我救主 這是我一 生\n[F]\n至寶"
+    },
+    {
+      title: "34. 你能體會",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F]\n1. 燦爛的新生命令人興奮\n2. 如今你仍未曾完全了解\n[Dm] [Gm] [C7]\n1. 只要你決心也有這生命\n2. 或試圖了解許多的疑問\n[Gm] [C7]\n1. 心靈的寂寞空虛主能填滿\n2. 祂要賜給你多於你所期望\n[Gm] [C7] [F]\n1. 祂賜你福樂使內心歡唱\n2. 比你原有的多出千萬萬\n[Bb] [Gm] [Am]\n(副)你能夠體會實在是美好奇妙\n[Gm] [C7] [F]\n你能夠體會是多麼壯觀\n[Gm] [C7] [Am]\n你能夠體會是多麼不可思量\n[G7] [C] [C7]\n你若願讓救主親手引領你"
+    },
+    {
+      title: "35. 當你求",
       originalKey: "F",
       timeSignature: "3/4",
       editor: "烏鴉Lin",
-      content: "[C][F][A7]榮耀[Dm]歸祂 [F]尊榮歸[Bb]祂 [Am]祂是[F]萬王之王\n[C7][F][A7]揚聲[Dm]歡唱 [F]高聲[C7]頌揚 讚美歸於主耶[F]穌"
+      content: "[C7] [Gm7] [F] [C7] [F]\n一日勞碌完畢 你禱告屈膝 有否將我惦記\n[C7] [Gm7] [F] [C7] [F]\n每日需用能力 所以請問你 是否將我惦記\n[Bb] [F] [C7] [F]\n哦 當你求 有否為我求 因我渴望主愛無憂\n[Bb] [F] [C7] [F]\n哦 當你求 有否為我求 是否微聲提我名代求"
     },
     {
-      title: "3. 全心來讚美你",
+      title: "36. 靠近十字架",
+      originalKey: "F",
+      timeSignature: "6/8",
+      editor: "烏鴉Lin",
+      content: "[F] [Bb] [F] [C7]\n1. 求主使我近十字架 在彼有生命水\n2. 我與主同釘十字架 和祂同葬同活\n3. 我立志不管別事 只傳耶穌基督\n4. 主禁我別有所誇 只誇救主十架\n[F] [Bb] [F] [C] [F]\n1. 由各各地山流下 白白賜人洗罪\n2. 今主因信住我心 我活是為主活\n3. 並祂為我釘十架 死後三日復活\n4. 世界對我已釘死 我以死對世界\n[F] [Bb] [F] [C7]\n(副) 十字架 十字架 永是我的榮耀\n[F] [Bb] [F] [C7F]\n我衆罪都洗清潔 惟靠耶穌寶血"
+    },
+    {
+      title: "37. 美哉主耶穌",
       originalKey: "F",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[F]全心來讚美你 [Dm7][Gm7]舉雙手表明[C]我愛你\n[Dm7]主你是我的一切 [Am7][Gm7]我尊榮你聖(1.[C]潔至高的名) [F][Bb][F]\n(2.[C]潔的名尊[Am7]榮你聖潔的[D]名 [Gm7]尊榮你聖潔[C]至高的[F]名)"
-    },
-    {
-      title: "4. 歡呼主耶穌",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[Bb][F]歡呼主耶穌 [F]歡呼以馬內[C]利\n[Dm]萬王之王 [Gm7]萬主之主明亮晨星\n[Bb]我要讚美[F7]你直到永永遠[F]遠\n[F]我要與你[D7]同作王[Gm7][Bb]直到永[C7]遠 [F]"
-    },
-    {
-      title: "5. 何等尊貴",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "(一)[F]尊貴[Gm7]何等尊貴 [Bb]萬王之王[C]萬主之主\n(二)[F]聖潔[Gm7]何等聖潔 [Bb]萬王之王[C]萬主之主\n(三)[F]耶穌我主耶穌 [Gm7]萬王之王 [Bb]萬主之[C]主\n(一)[Bb][F]何等[F]尊 尊貴 [Gm7]何等尊貴\n(二)[Bb][F]何等[F]聖潔聖潔 [Gm7]何等聖潔\n(三)[Bb][F]我主[F]耶穌耶穌 [Gm7]我主耶穌\n[Bb]萬王之王[C]萬主之主 我敬拜[F]你"
-    },
-    {
-      title: "6. 尊崇主耶和華",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[F][Am7]尊崇主耶和[F]華 [Am7]尊崇主耶和[F7]華\n[Am7][Dm7]來在祂腳[Am7][Dm7]前下拜 [Bb]在祂腳前下拜\n[Gm7][C7][Dm]祂[Bbm]本為[Gm7]聖 [C7]祂本為[F]聖"
-    },
-    {
-      title: "7. 祂是萬王之王",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[F]祂是萬王之王 [F]祂是萬主之[C7]主\n[F]祂名是耶穌 [Eb]耶穌 [D]耶穌 [Db]耶穌\n[F]哦祂是我[C7]王"
-    },
-    {
-      title: "8. 從太陽出來之地",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[F]從太陽出來之[F]地 直到日落處的那邊 [C7]主的[F]名當讚美[F]的\n[F]從太陽出來之[F]地 直到日落處的那邊 [C7]主的[F]名當讚美[F7]的\n[Bb]讚美主名 [F][F+]神的兒女[Bb]要讚[C]美 讚美[F]主的聖[F7]名\n[Bb]讚美主聖名[F]從[F+]今時直到永[Bb]遠 讚美[C7]主的聖[F]名"
-    },
-    {
-      title: "9. 和散那",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[F]和散那 [Gm]和散那 [Bb]和散那歸於[C]神羔[F]羊\n[F]和散那 [Gm]和散那 [Bb]和散那歸於[C]神羔[F]羊"
-    },
-    {
-      title: "10. 你是榮耀君王",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[F]你是榮耀的君王[Bb]你是和平之[C7]主\n[A7]你是天地萬有主[Dm]宰 [Bb]唯有你聖潔公[C7]義\n[A7]天使都向你跪[Dm]下 [Bb]敬拜尊崇[C7]你\n[F]因為在你有永生之[Bb]道 [C7]你是主耶[Dm]穌 [Bb]基[C7]督\n[F]和散那歸於大衛的[Dm]子孫 [Bb]和散那歸於萬王之[C7]王\n[F]願榮耀歸於[Dm]至高[Bb]神 耶[C7]穌是彌賽[F]亞"
-    },
-    {
-      title: "11. 不再定罪",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[F]在基[Am7]督裡面[Dm]你在基[F]督裡面[Dm]就不再被[Gm]定[Bb]罪\n[F]不再[C]定罪 (副)因為在基[F/A]督裡面[F/C]生命[Am7]聖靈的[Dm7]律\n[Am7]要釋[Dm7]放你 [F]脫離[Am]罪和[C]死的[Dm]律[C/D][Dm][C]"
-    },
-    {
-      title: "12. 來慶賀主耶穌",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[F]來歡呼慶賀主耶穌[F]來歡呼慶賀主耶穌[F]來歡呼慶賀主耶穌\n[C]來歡[Dm]呼慶賀主耶穌[C]祂已復[Dm]活祂已復[C]活\n[Dm]從[Bb]今時直[C]到永[Dm]遠祂已復[C]活祂已復[F]活\n同來歡呼慶賀 主耶穌基督已復活"
-    },
-    {
-      title: "13. 當轉眼仰望耶穌",
-      originalKey: "F",
-      timeSignature: "3/4",
-      editor: "烏鴉Lin",
-      content: "1.[F]當轉眼仰望耶[C7]穌 [Dm]定睛在[F7]祂奇妙慈[Bb]容[Gm]\n[C]在救主[C7]榮耀恩典大光[F]中 [C7][F]世上事[Dm]必然顯為虛[F]空[Bb][C7][F]\n2.[F]當不住仰望耶[C7]穌[Dm]不容祂[F7]人奪取你[Bb]心[Gm]\n[C]每時刻[C7]祂必賜下新能[F]力 [C7][F]直到爭[Dm]戰完畢得勝[F]歸[Bb][C7][F]"
-    },
-    {
-      title: "14. 最大的財富",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "1.[F]我最大的財富就是[C]神 [F]我最大的財富就是[Bb]神[Am]\n[Bb]有他我[F]滿足 [Dm]再也不缺[Bb]乏[C]我最大的財富就是[F]神\n2.[F]我最大的保障就是[C]神 [F]我最大的保障就是[Bb]神[Am]\n[Bb]有他我[F]平安 [Dm]再也不懼[Bb]怕[C]我最大的保障就是[F]神\n3.[F]我最大的喜樂就是[C]神 [F]我最大的喜樂就是[Bb]神[Am]\n[Bb]有他我[F]喜樂 [Dm]再也不消[Bb]沈[C]我最大的喜樂就是[F]神"
-    },
-    {
-      title: "15. 進入幔內",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[F]進入幔內[Gm]如今我[C7]來 [F]進入[Gm]至聖所\n[E7][F]瞻仰我[C7]主榮[D7]面 [Gm]何等榮[C7]美彰顯\n[A7][Dm]無一能夠[Gm]相[C7]比 [F]我敬拜主我神\n[F]進入幔內"
-    },
-    {
-      title: "16. 當趁著年輕",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[F][Dm]當趁著[Am]年輕記念[D/Bb]造你[C]的主 [F][Dm]當趁著[Bb]年輕記念[G]造你[Bb]的主[C]\n[F]不要讓時[Dm]光 白白[F]的流走 [Dm][Gm]時光一去[Am]不回[Dm]頭\n[C][F]把握好[Am]每一天[Bb]燦爛的瞬[F]間 [G]與神同享甜蜜\n[Bb][C][F]當趁著年輕 記念造你的主[F][Dm]時光一去不回頭[G][Bb][C][F]"
-    },
-    {
-      title: "17. 全心讚美",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[F]用嘴唇來[Bb]讚美你[Gm7]用我[C7]口來歌頌你\n[F]我雙眼渴望[Gm7]看見你[A7]向我[Dm]彰顯[D]\n[Bb]願我耳聆聽[Gm]你聲音[C7]我雙[F]手領受你大愛\n[Gm7]我的全心[A7]完全降服於[Dm]你\n[Gm]凡那在[C7]我裡面的[F]讚美[Bb]你聖[Gm]名 [A][Dm][D7]\n[Gm7]凡那在[C7]我裡面的[F]讚美[Bb]你聖[Gm]名 [A7][Dm]"
-    },
-    {
-      title: "18. 耶穌正在這裡",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "(一)[Gm7]耶穌正[C7]在這裡 [F][Am7][Dm]祂同在滿有榮耀\n[Gm7]呼召[C7]祂的百姓[F]近前[Dm7]來 [F]\n(二)[Gm7]耶穌正[C7]在這裡 [F][Am7][Dm]祂同在滿有榮耀\n[Gm7]呼召[C7]祂的百姓[F]近前來\n(副)[F][Gm7][C7]主耶[F]穌 請你[Dm]來主聖靈來運行\n[Gm7][C7][F][Gm7][Am7]主耶穌我們仰望你\n[F][Gm7][C7][F]主耶[F]穌請你[Dm]來主聖靈來運行\n[Gm7][C7][F]主耶穌我們仰望你"
-    },
-    {
-      title: "19. 合而為一",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[F]我們雖許多[Gm]卻是一個身[Bb]體在[C7]基督裏[C7]合而為[F]一(反覆)"
-    },
-    {
-      title: "20. 主是平安",
-      originalKey: "F",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[F]主是平安[Gm7]祂已拆毀中[F]間隔斷的[C7]牆\n[F]主是平安[Gm7]主是平[C7]安(二次) [F]\n[F7][Bb]將憂慮[Gm7]卸給祂 [Am7][F#]因祂[Gm7]顧念你 [C7]主是平安\n[F]主是平安(反覆)"
+      content: "[F] [C7F] [BbF] [F] [Bb] [F]\n1. 美哉主耶穌 宇宙萬物主宰 祂是真神\n2. 陽光多美麗 月光更覺清輝 繁星點點\n[BbF] [C7] [F] [BbF] [C7] [F]\n1. 降世為人 我心所景仰 我靈所崇尊\n2. 燦爛閃耀 耶穌更光明 耶穌更皎潔\n[C7F]\n1. 是我榮耀冠冕歡欣\n2. 天使榮光不足比較"
     }
   ];
