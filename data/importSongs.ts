7e7c02f95307bd407ee4ca7e5b41183e33020284
woodylin0403 <coolcrow0403@gmail.com>
2026-03-09 08:19:57 +0800

    匯入歌曲1

--- data/importSongs.ts
+++ data/importSongs.ts
@@ -1,37 +1,72 @@
 export const importSongs = [
     {
-      title: "1. 舉目仰望",
-      originalKey: "C",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[C]舉目仰望 [G]仰望耶穌 [Am]仰望[F]主耶穌榮[C]光\n[G7]舉目仰望 [C]仰望耶[G]穌 仰望[Am]主耶[F]穌榮[C]光\n[Am]主是光 [F]聖潔與[G7]真理 全能[Em7]神主的[Am7]榮[F]光 [G]已臨到[C]我們[C7]\n齊來[F]宣揚 [G]宣揚我[Em7]主[Am7] 因[Dm]為祂的[G]慈愛永遠長[C]存 [F][G]\n齊來宣揚 宣揚[Em7]我主[Am7] 因為祂的[Dm]慈愛[G7]永遠長[C]存"
-    },
-    {
-      title: "2. 每當我瞻仰你",
-      originalKey: "C",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[C]每當我瞻仰你[F]至聖榮[Dm]面\n每當我[C]在愛中仰望你\n[F]在你榮耀光[Dm]中 所有一[C]切都失去光[Dm]彩[G]\n[C]何等喜樂當我[F]進入你心[Dm]意\n我全[C]心成為你愛的寶座\n[F]在你榮耀光[Dm]中 所有一[C]切都失去光[Dm]彩[G]\n我敬拜[Em]你 我敬拜[Am]你\n我[Dm]一生活著為要[G7]敬拜[C]你[F][G]\n我敬拜[Em]你 我敬拜[Am]你\n我[Dm]一生活著為要[G7]敬拜[C]你"
-    },
-    {
-      title: "3. 讚美救主耶穌",
+      title: "6. 我留下平安",
       originalKey: "C",
       timeSignature: "3/4",
       editor: "烏鴉Lin",
-      content: "[C]讚美我救主耶[G7]穌 榮耀我救主耶[C]穌\n感謝我救主耶[G]穌 將榮耀歸給主耶[C]穌\n\n[C]主的聖靈充滿[G7]我 主的寶血潔淨[C]我\n靠我主聖名得[G]勝 將一切榮耀歸父[C]神\n\n(副) 唱讚美[F]主 哈利路[G]亞\n[C]阿們讚美救主耶[F]穌 [G]\n唱讚美[C]主 [F]哈[G]利路[C]亞\n[C]阿們[F]阿們 [C]哈利路亞 [G]阿[C]們"
+      content: "[C]我留下平[Em]安給你[Dm]們 [G]我將我的平[C]安賜給[Em]你\n[Dm]我所賜[F]的不像[Em]世人所[Am]賜的 [Dm]你們心[G7]裏不要[Em]憂[Am]愁\n[Dm]我留下平[G7]安給你們[C][F][C]\n(2)慈愛 (3)喜樂"
     },
     {
-      title: "4. 我的心哪來稱頌主",
-      originalKey: "C",
-      timeSignature: "12/8",
-      editor: "烏鴉Lin",
-      content: "[Dm]我的心哪[G]來稱頌[C]主[Am]\n[Dm]我的心哪[G]來稱頌[C]主\n[Am7]我的心哪[Dm]來稱頌[G]主聖[C]名[E7]\n[F]凡在我[Dm7]裏面的都[C]稱頌主\n[F]我的心[Dm7]來稱頌[C]主[F]\n哦[C]我的心哪[Dm]來稱頌[Em]主\n[F]我的心哪[C]來稱頌主\n[Am7]凡在我裹[G7]面的都稱頌[C]主"
-    },
-    {
-      title: "5. 耶穌耶穌我心樂歌",
+      title: "7. 機遇",
       originalKey: "C",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[C]耶穌[G7]耶穌 [F]是我心[G7]樂[C]歌\n[C]耶穌[G7]耶穌 [F]步步[G7]領導[F]路\n[F]祂時常保[G7]守我 [Em]祂必不離開[G]我\n一直[G7]到天家那快樂[C]處\n[C]耶穌[Em]耶穌 [F]最親[G7]愛朋[F]友\n[C]耶穌[G7]耶穌 [F]光照我四[C]周[G7]\n我們高聲歌頌祂慈[Em]愛\n群[F]聚在祂聖旗[G7]下行[C]走"
+      content: "[Am]1. 像青空白雲連綿不[C]停 像水面浮[C]萍漂流不[F]定\n[Am]人生的機[Dm]遇稍縱即[C]逝 切莫等[C]待切莫遲[G7]疑切莫因[C]循\n[Am]2. 像天空繁星忽現忽[C]隱 像江上帆[C]影迎向光[F]明\n[Am]人生的機[Dm]遇稍縱即[C]逝 我心嚮[C]往我靈渴[G7]羨我願追[C]尋"
+    },
+    {
+      title: "8. 主啊!是否是我",
+      originalKey: "C",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[C]海洋與天[F]空的主 [C]垂聽我們[F]的哀[C]慟\n[G]處在[C]罪惡[Am]黑暗者 [Dm]你必[F]拯[Dm]救[G][C]\n[F]創造了天[C]上星星 [C]發出亮[F]光除[C]黑[G]暗\n[C]誰能[Am]傳揚[Dm]這亮光 [F]要差[Dm]遣[G]誰\n[G]我的[C]主啊[F]是否是[C]我 [Dm7]主啊我在這裡差遣[G]我\n[C]照著話語 [F]願意跟[C]從 [Dm7]主的慈[G]愛我願去傳[C]揚"
+    },
+    {
+      title: "9. 彼此相愛",
+      originalKey: "C",
+      timeSignature: "3/4",
+      editor: "烏鴉Lin",
+      content: "[C]要彼此相[G7]愛 因神就是[F]愛[C][F]\n[C]愛神的是從神[C]生並認識[G7]神\n[C]沒有愛[G7]心的就不認識[F]神\n[C]因神就是[F]愛 [C]神[G7]就是[C]愛"
+    },
+    {
+      title: "10. 像我這樣",
+      originalKey: "C",
+      timeSignature: "6/4",
+      editor: "烏鴉Lin",
+      content: "[G7]1. 像我這[C]樣重重罪[G7]愆 誰能拯[F]救誰能赦[C]免\n[C]主愛高[F]深召我重[C]生 救主耶[G7]穌我來我[C]來\n[G7]2. 像我這[C]樣你肯收[G7]留 接納饒[F]恕赦我愆[C]尤\n[C]主我相[F]信你的應[C]許 救主耶[G7]穌我來我[C]來"
+    },
+    {
+      title: "11. 我知誰掌管明天",
+      originalKey: "C",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[C]1. 我不知明[C7]日將如[F]何 每時刻安[C]然度過\n[F]我不求明[C]天的陽[Am]光 因明[G7]天或轉隱暗\n[C]我不為將[C7]來而憂[F]慮 因我知主[C]所應許\n[Am]今天我必[G7]與主同[C]行 祂深知前途光景\n[C]2. 我不知明[C7]日將如[F]何 或遭遇貧[C]苦飢餓\n[F]那看顧麻[C]雀的恩[Am]主 必隨[G7]我時刻看顧\n[C]我前程雖[C7]經歷水[F]火 或快樂或[C]有災禍\n[Am]但我主引[G7]導我路[C]途 祂寶血將我塗抹\n\n[F]副：許多事明[C]天將臨到 [Am]許多事難[G7]以明瞭\n[C]但我知主[F]掌握明[C]天 祂[G7]必要領我向[C]前"
+    },
+    {
+      title: "12. 慈愛牧者",
+      originalKey: "C",
+      timeSignature: "6/4",
+      editor: "烏鴉Lin",
+      content: "[Dm]1. [Dm7]大愛牧[G7]人 [C]你知道我迷了路 大愛牧人你[G7]關心我入歧[C]途\n[Dm]2. [Dm7]大愛牧[G7]人 [C]我的救主和嚮導 大愛牧人我[G7]要在你旁到永[C]遠\n\n[F]副：你前來尋我 [C]雙手[A7]抱我 [Dm]懷抱我[G7]領我回[C]家[C7]\n[F]無敵能害我 [C]使我[A7]驚恐 [Dm]我不再[D7]憂愁[G]嘆[G7]息"
+    },
+    {
+      title: "13. 你是我的神",
+      originalKey: "C",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[C]神[G7]啊 你[C]是我的[C7]神 [F]我要切切的[G7]尋求[C]你\n在乾[F]旱[G7]疲乏[C]無水之[Am]地 [Dm]我[D7]渴想[G7]你 [F]我[G7]的心切慕[C]你[G7]\n[C]神[G7]啊 你[C]是我的[C7]神 [F]我要切切的[G7]尋求[C]你\n在乾[F]旱[G7]疲乏[C]無水之[Am]地 [Dm]我渴想你[G7]我的心切慕[C]你[G7][C]"
+    },
+    {
+      title: "14. 親近更親近",
+      originalKey: "C",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[C]親近更親[Em]近 [F]近主心[C]懷\n[F]親愛之救[C]主 引[Am]我近前[G7]來\n[C]雙手扶持[Em]我 [F]靠主胸[G7]前\n[C]如在穩靜[G7]港 掩[C]蔽我平[F]安[C]\n[D7]如在[G7]穩靜[C]港 掩蔽我平安"
+    },
+    {
+      title: "15. 領我啊領我",
+      originalKey: "C",
+      timeSignature: "3/4",
+      editor: "烏鴉Lin",
+      content: "[C]黑暗愁雲阻擋我道路 [G]我[G7]怎能找到[C]路\n[C]前面道路錯綜而迷離 [Am]領我求主領[G]我[C]\n[C]領我啊領[F]我 決[Dm]不走錯誤[G7]迷失道[C]路\n[C]領我啊領[F]我 領[Dm]導我直到[G7]我抵天家[C]路"
     }
   ];
