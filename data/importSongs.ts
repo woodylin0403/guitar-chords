c1d89348d8c628d51fd092c0a79054d084c6bad4
woodylin0403 <coolcrow0403@gmail.com>
2026-03-09 11:49:08 +0800

    song9

--- data/importSongs.ts
+++ data/importSongs.ts
@@ -1,142 +1,72 @@
 export const importSongs = [
     {
-      title: "41. 我曾俯伏在耶穌腳下",
+      title: "61. 主是平安",
       originalKey: "D",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[D]我曾俯伏在[A/C#]耶穌腳下 [Bm]我曾俯伏在[D/A]耶穌腳下\n[G]我曾俯伏在[D/F#]耶穌腳下 [Em]心中充滿平安[A]\n[D]主耶穌啊[A/C#]我感謝你 [Bm]主耶穌啊[D/A]我讚美你\n[G]主耶穌啊[D/F#]我稱頌你 [A]你名何其美[D]"
+      content: "[D]主是平安 [Bm]祂賞賜平安 [G]主是平安 [A]祂賞賜平安\n[D]我心中有平安 [G]我心中有平安 [A]主耶穌是我[D]平安"
     },
     {
-      title: "42. 我必須有主",
+      title: "62. 讚美耶和華",
       originalKey: "D",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[D]我必須有主 [A]我必須有主 [G]主若不[A]同在 [D]我就沒[A]有路\n[D]我必須有主 [A]我必須有主 [G]主若不[A]同在 [D]我就沒[A]有路"
+      content: "[D]讚美耶和華 [Bm]讚美耶和華 [G]讚美耶和華 [A]哈利路亞\n[D]讚美耶和華 [Bm]讚美耶和華 [G]讚美耶[A]和華[D]"
     },
     {
-      title: "43. 信賴耶穌心中就有平安",
+      title: "63. 耶和華是我的牧者",
       originalKey: "D",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[D]信賴耶穌[A]心中就有平安 [G]信賴耶穌[A]心中就有平安\n[D]信賴耶穌[G]心中就有平安 [A]心中就有[D]平安"
+      content: "[D]耶和華是我的[F#m]牧者 [Bm]我必不致缺[F#m]乏\n[G]祂使我躺臥在[D]青草地上 [Em7]領我在水邊[A]\n[D]耶和華是我的[F#m]牧者 [Bm]我必不致缺[F#m]乏\n[G]祂使我靈魂[A]甦醒 [D]"
     },
     {
-      title: "44. 我知我何往",
-      originalKey: "D",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[D]我知我何往 [Bm]我知我何往 [G]因主耶穌[A]祂正牽引[D]我\n[D]我知我何往 [Bm]我知我何往 [G]因主耶穌[A]祂正牽引[D]我"
-    },
-    {
-      title: "45. 草鞋",
-      originalKey: "D",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[D]一雙破草鞋 [Bm]走在曠野地 [G]雖然有風沙 [A]我不覺孤單\n[D]因為有耶穌 [Bm]與我同走路 [G]草鞋雖破[A]主愛永長[D]存"
-    },
-    {
-      title: "46. 禱",
-      originalKey: "D",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[D]當我來到[F#m]你面前 [Bm]低頭向你[F#m]禱告\n[G]求你垂聽[D]我的聲音 [Em7]求你醫治[A]我的心\n[D]主啊 我需要[F#m]你 [Bm]主啊 我仰望[F#m]你\n[G]願你愛[A] 充滿我[D]心"
-    },
-    {
-      title: "47. 願耶和華賜福與你",
-      originalKey: "D",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[D]願耶和華[F#m]賜福與你 [G]願耶和華[A]保護你\n[D]願耶和華[F#m]使祂的臉光[G]照你 [A]賜你平安\n[G]願耶和華[A]賜福與你 [F#m]保護你[Bm]直到永遠\n[Em7]願你出[A]也平安 [D]入也平安"
-    },
-    {
-      title: "48. 在這個時刻",
-      originalKey: "D",
-      timeSignature: "6/8",
-      editor: "烏鴉Lin",
-      content: "[D]在這個時[Bm]刻 我[Em]向主唱新[A]歌 [Em]向主唱出[A] 讚美[D]的歌 [A]\n[D]在這個時[Bm]刻 我[Em]高舉雙[A]手 [Em]向主高舉[A] 雙手[D]讚美\n[G]唱著[A]主 [D]哈利路[Bm]亞 [G]唱著[A]主 [D]哈利路[Bm]亞\n[G]唱著[A]主 [D]哈利路[Bm]亞 [G]讚美[A]主 [D]"
-    },
-    {
-      title: "49. 來向主歌唱",
-      originalKey: "D",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[D]來向主歌唱 [Bm]來向主歡呼 [G]來向主高[A]舉讚美\n[D]來向主歌唱 [Bm]來向主歡呼 [G]來向主[A]高聲讚[D]美"
-    },
-    {
-      title: "50. 永在我心",
+      title: "64. 我靈歌唱",
       originalKey: "D",
       timeSignature: "3/4",
       editor: "烏鴉Lin",
-      content: "[D]主耶穌[Bm]永在我心 [G]主耶穌[A]永在我心\n[D]主耶穌[Bm]永在我心 [G]永不[A]分離[D]"
+      content: "[D]我靈歌唱 [Bm]我靈歌唱 [G]讚美主[A]大能\n[D]我靈歌唱 [Bm]我靈歌唱 [G]讚美[A]主愛[D]"
     },
     {
-      title: "51. 願我就近聖靈江河",
+      title: "65. 因為祂活著",
+      originalKey: "D",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[D]因祂活著 [G]我能面對明天 [D]因祂活著 [Bm]不再懼怕 [E7][A7]\n[D]我深知道 [G]祂掌管明天 [D]生命充滿[A]了 希望 [G]只因[A]祂活[D]著"
+    },
+    {
+      title: "66. 耶穌愛我",
+      originalKey: "D",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[D]耶穌愛我 [Bm]萬事盡好 [G]耶穌愛我 [A]我必得救\n[D]耶穌愛我 [Bm]永遠愛我 [G]主耶穌[A]愛我[D]"
+    },
+    {
+      title: "67. 向耶和華唱新歌",
+      originalKey: "D",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[D]全地都要向耶和華[Bm]唱新歌 [G]全地都要向耶和華[A]唱新歌\n[D]要向耶和華歌唱 [Bm]稱頌祂的名 [G]天天傳揚[A]祂的救[D]恩"
+    },
+    {
+      title: "68. 你要認識神",
+      originalKey: "D",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[D]你要認識神 [Bm]就得平安 [G]福氣也必 [A]臨到你身\n[D]你要認識神 [Bm]就得平安 [G]主愛[A]必臨到[D]"
+    },
+    {
+      title: "69. 哈利路亞讚美主",
+      originalKey: "D",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[D]哈利路亞 [Bm]哈利路亞 [G]讚美我的[A]主\n[D]哈利路亞 [Bm]哈利路亞 [G]讚美[A]我的[D]主"
+    },
+    {
+      title: "70. 願主與你同在",
       originalKey: "D",
       timeSignature: "3/4",
       editor: "烏鴉Lin",
-      content: "[D]願我就近[Bm]聖靈江河 [G]渴慕主[A]生命活水\n[D]洗淨我心[Bm]更新我靈 [G]使我[A]像你[D]"
-    },
-    {
-      title: "52. 神的聖靈",
-      originalKey: "D",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[D]神的聖靈[F#m]降臨在這地 [G]充滿我[A]的心\n[D]神的聖靈[F#m]降臨在這地 [G]使我得[A]生命\n[G]聖靈 [A]聖靈 [F#m]我們[Bm]歡迎你 [Em7]聖靈 [A]聖靈 [D]我們歡迎你"
-    },
-    {
-      title: "53. 讚美祭",
-      originalKey: "D",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[D]我們進入[F#m]祂的門 [G]稱謝讚美[A]祂\n[D]我們進入[F#m]祂的院 [G]歌頌祂[A]聖名\n[G]獻上[A]讚美祭 [F#m]獻上[Bm]感恩祭 [Em7]主啊[A]我們稱謝[D]你"
-    },
-    {
-      title: "54. 不忘主愛",
-      originalKey: "D",
-      timeSignature: "3/4",
-      editor: "烏鴉Lin",
-      content: "[D]我不忘記[Bm]你的愛 [G]我不忘記[A]你的恩\n[D]我要一生[Bm]跟隨你 [G]永不[A]分離[D]"
-    },
-    {
-      title: "55. 如鹿切慕溪水",
-      originalKey: "D",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[D]神啊 我的心[A/C#]切慕[Bm]你 [D/A]如鹿[G]切慕[A]溪水\n[D]唯有你是我[A/C#]心所[Bm]愛 [D/A]我渴[G]慕來[A]敬拜[D]你\n[Bm]你是我的[D/A]力量和盾[G]牌 [D/F#]我靈[Em]只向[F#]你投靠\n[D]唯有你是我[A/C#]心所[Bm]愛 [D/A]我渴[G]慕來[A]敬拜[D]你"
-    },
-    {
-      title: "56. 全能的上帝",
-      originalKey: "D",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[D]全能的上帝 [Bm]全能的上帝 [G]在祂沒有[A]難成的事\n[D]全能的上帝 [Bm]全能的上帝 [G]在祂[A]沒有難成[D]的事"
-    },
-    {
-      title: "57. 忠心順服歌",
-      originalKey: "D",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[D]我要忠心[Bm]順服主 [G]我要忠心[A]跟隨主\n[D]無論主往[Bm]哪裡去 [G]我也[A]必跟[D]隨"
-    },
-    {
-      title: "58. 天父阿爸天父",
-      originalKey: "D",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[D]天父阿爸[F#m]天父 [G]我愛你[A]我敬拜你\n[D]天父阿爸[F#m]天父 [G]我愛你[A]我敬拜[D]你\n[G]你愛我 [A]你疼我 [F#m]你領我 [Bm]你導我 [Em7]天父 [A]我愛[D]你"
-    },
-    {
-      title: "59. 開我的眼主",
-      originalKey: "D",
-      timeSignature: "12/8",
-      editor: "烏鴉Lin",
-      content: "[D]開我的眼[A]主 [Bm]開我的眼[F#m]主\n[G]使我能[D]看見你 [Em]使我能[A]認識你\n[D]開我的眼[A]主 [Bm]開我的眼[F#m]主\n[G]使我能[A]看見[D]你"
-    },
-    {
-      title: "60. 榮耀耶穌",
-      originalKey: "D",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[D]榮耀耶穌 [Bm]榮耀耶穌 [G]榮耀歸給[A]聖潔羔羊\n[D]榮耀耶穌 [Bm]榮耀耶穌 [G]榮耀[A]歸給[D]主"
+      content: "[D]願主與你[Bm]同在 [G]直到永[A]遠\n[D]願主與你[Bm]同在 [G]直到[A]再相[D]會"
     }
   ];
