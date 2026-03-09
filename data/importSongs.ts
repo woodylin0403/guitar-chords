dde99f32b499a7932433251f2c59f8feaafe4521
woodylin0403 <coolcrow0403@gmail.com>
2026-03-09 10:52:37 +0800

    歌詞15

--- data/importSongs.ts
+++ data/importSongs.ts
@@ -1,72 +1,30 @@
 export const importSongs = [
     {
-      title: "11. 這一生最美的祝福",
+      title: "21. 我們獻上讚美",
       originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[E]在無數的黑夜[G#m]裡 [A]我用星星畫出[E]祢\n[A]祢的恩典如晨[E]星 [F#m]讓我真實的見[B]到祢\n[E]在我的歌聲[G#m]裡 [A]我用音符讚美[E]祢\n[A]祢的愛大過[E]諸天 [F#m]慈愛[B]高過雲[E]霄\n[E]這一生最美[G#m]的祝福 [A]就是能認識[E]主耶穌\n[A]這一生最美[E]的祝福 [F#m]就是能信靠[B]主耶穌\n[E]走在高山深[G#m]谷 [A]祂會伴我[E]同行\n[A]我知道[E]這是最美[F#m]的祝[B]福 [E]"
+      content: "[E]我們獻上[G#m]讚美 [A]在祢的聖所[B]裡\n[E]我們獻上[G#m]尊榮 [A]在祢的寶座[B]前\n[A]因祢是[B]萬王之王 [G#m]祢是[C#m]萬主之主\n[F#m]祢配得[B]一切的[E]讚美\n[A]因祢是[B]萬王之王 [G#m]祢是[C#m]萬主之主\n[F#m]祢配得[B]一切的[E]讚美"
     },
     {
-      title: "12. 最知心的朋友",
+      title: "22. 讓我親近祢",
       originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[E]主祢是[A]我 最知心的[E]朋友 [C#m]主祢是[F#m]我 最親愛的[B]伴侶\n[E]我的心在[A]天天追尋[E]祢 [F#m]渴望[B]見到祢的[E]面\n[A]在我人生的[B]每一個台[G#m]階 [C#m]在我人生的[F#m]每一個小[B]站\n[E]祢的手[A]總是在攙拉[E]著我 [F#m]把我帶[B]在祢身[E]邊"
+      content: "[E]讓我親近[A]祢 [B]不要離開[E]我\n[C#m]我願獻上[F#m]我所有 [B]求祢聽我的[E]呼求\n[E]祢是我所[A]想 [B]祢是我所[E]求\n[C#m]無人能與[F#m]祢相比 [B]祢是我的一[E]切\n[E]引導我[A]靠近祢 [B]引導我[E]靠近祢\n[C#m]讓我能[F#m]更深 [B]認識[E]祢"
     },
     {
-      title: "13. 耶穌，我的耶穌",
+      title: "23. 聖靈的江河",
       originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[E]耶穌[A]我的耶[E]穌 [A]祢的愛[E]將我包[B]圍\n[E]耶穌[A]我的耶[E]穌 [F#m]祢的愛[B]將我包[E]圍\n[A]我要[B]一生跟隨[G#m]祢 [C#m]我要[F#m]一生服事[B]祢\n[E]耶穌[A]我的耶[E]穌 [F#m]祢的愛[B]將我包[E]圍"
+      content: "[E]聖靈的江河 [A]正流過這[E]地\n[C#m]聖靈的江河 [F#m]帶來了生[B]命\n[E]聖靈的江河 [A]醫治了萬[E]民\n[C#m]聖靈的江河 [F#m]正[B]流過這[E]地\n[A]湧流[B]湧流 [G#m]聖靈的[C#m]江河\n[F#m]湧流在[B]我們的心[E]中[E7]\n[A]湧流[B]湧流 [G#m]聖靈的[C#m]江河\n[F#m]湧流在[B]我們的心[E]中"
     },
     {
-      title: "14. 祢的愛",
+      title: "24. 榮耀的呼召",
       originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[E]祢的愛 [G#m]長闊高深 [A]我不能[E]測透\n[C#m]祢的愛 [F#m]完全包容 [A]我所有[B]的軟弱\n[E]祢的愛 [G#m]長闊高深 [A]我不能[E]測透\n[C#m]祢的愛 [F#m]完全包容 [B]我所有[E]的軟弱\n[A]我要[B]讚美祢 [G#m]我要[C#m]稱頌祢\n[F#m]因祢的[B]愛奇妙無[E]比 [E7]\n[A]我要[B]讚美祢 [G#m]我要[C#m]稱頌祢\n[F#m]因祢的[B]愛奇妙無[E]比"
-    },
-    {
-      title: "15. 相信有愛就有奇蹟",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]黑夜過[A]後 總有[E]日出 [A]暴風雨[E]過後 總有[B]天晴\n[C#m]在絕望[A]中 不要[E]放棄 [F#m]因為有[B]愛 就有[E]奇蹟\n[A]相信[B]有愛 [G#m]就有奇[C#m]蹟 [F#m]上帝的[B]愛 充滿[E]在這裡\n[A]相信[B]有愛 [G#m]就有奇[C#m]蹟 [F#m]只要相[B]信 就有[E]奇蹟"
-    },
-    {
-      title: "16. 愛我的天父",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]有一位[A]愛我的天[E]父 [C#m]祂永遠[F#m]不放棄[B]我\n[E]祂呼喚[A]著我的名[E]字 [F#m]祂溫柔[B]地對我[E]說\n[A]我愛祢 [B]我愛祢 [G#m]我的孩[C#m]子\n[F#m]我永遠 [B]都不撇下[E]祢 [E7]\n[A]我愛祢 [B]我愛祢 [G#m]我的孩[C#m]子\n[F#m]我永遠 [B]都不撇下[E]祢"
-    },
-    {
-      title: "17. 豐盛的應許",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]祢有一[A]個豐盛的[E]應許 [C#m]為愛祢的[F#m]人所預[B]備\n[E]眼睛未[A]曾看見 耳朵[E]未曾聽見 [F#m]人心也[B]未曾想[E]到\n[A]神啊祢[B]是何等偉[G#m]大 [C#m]祢的慈[F#m]愛高及諸[B]天\n[A]神啊祢[B]是何等偉[G#m]大 [C#m]我要一[F#m]生 [B]讚美[E]祢"
-    },
-    {
-      title: "18. 我要向高山舉目",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[|] [E] [|] [A] [|] [E] [|] [B] [|]\n[E]我要向高山[G#m]舉目 [A]我的幫助從何而[B]來\n[E]我的幫助 [G#m]從造天地的 [A]耶和[B]華而[E]來\n[C#m]祂必不叫你[G#m]的腳搖動 [A]保護你的必不打[B]盹\n[C#m]保護以色[G#m]列的 [A]也不打盹[B]也不睡[E]覺"
-    },
-    {
-      title: "19. 全然為祢",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]將我全[A]人 獻上當做[E]活祭 [C#m]這是聖潔[F#m]是神所喜[B]悅的\n[E]將我全[A]心 獻上當做[E]活祭 [F#m]這是我[B]理所當[E]然的\n[A]全然為[B]祢 [G#m]全然為[C#m]祢 [F#m]我要一[B]生 全然為[E]祢\n[A]全然為[B]祢 [G#m]全然為[C#m]祢 [F#m]我要一[B]生 全然為[E]祢"
-    },
-    {
-      title: "20. 求主充滿我",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]求主[A]充滿我 [B]求主[E]充滿我\n[C#m]用祢的[F#m]聖靈 [B]來充滿[E]我\n[E]求主[A]充滿我 [B]求主[E]充滿我\n[C#m]用祢的[F#m]愛情 [B]來充滿[E]我\n[A]我渴[B]慕祢 [G#m]我尋[C#m]求祢\n[F#m]求主[B]充滿我[E]心 [E7]\n[A]我渴[B]慕祢 [G#m]我尋[C#m]求祢\n[F#m]求主[B]充滿我[E]心"
+      content: "[E]祢的呼召 [G#m]何等榮耀 [A]我願一生 [B]跟隨祢\n[E]祢的應許 [G#m]永不改變 [A]我願一生 [B]信靠祢\n[C#m]雖有高山 [G#m]雖有低谷 [A]祢的恩典 [B]夠我用\n[C#m]我願背起 [G#m]我的十架 [A]跟隨祢 [B]到底\n[E]主我願 [G#m]獻上自己 [A]為主做 [B]榮耀見證\n[E]主我願 [G#m]獻上自己 [A]一生為 [B]祢而[E]活"
     }
   ];
