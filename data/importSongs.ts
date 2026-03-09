bc9b93f904b6f4e84e8d05e83f76a798b86d59b8
woodylin0403 <coolcrow0403@gmail.com>
2026-03-09 10:05:22 +0800

    歌詞7

--- data/importSongs.ts
+++ data/importSongs.ts
@@ -1,72 +1,72 @@
 export const importSongs = [
     {
-      title: "1. 耶和華作王",
-      originalKey: "Am",
+      title: "1. 一切歌頌讚美",
+      originalKey: "D",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[Am]耶和華作王 [G]耶和華作王 [F]祂必作王到[E7]永遠\n[Am]耶和華作王 [G]耶和華作王 [F]祂必[G]作王到[Am]永遠\n[C]天上萬軍[G]大聲歡呼 [F]全地萬民[E7]也喜樂\n[Am]耶和華作王 [G]耶和華作王 [F]祂必[G]作王到[Am]永遠"
+      content: "[D]一切歌頌[F#m]讚美 [G]都歸我主[A7]我的神\n[D]你是配得[F#m]歌頌與[G]讚美 [A7]\n[D]我們高聲[F#m]呼喊 [G]高舉耶穌[A7]之名\n[D]哈利路[F#m]亞 [G]讚美主[D]哈利路亞 [A7]\n[D]哦 讚美主[F#m]哈利路[G]亞 [A7]哈利路[D]亞"
     },
     {
-      title: "2. 以色列的聖者",
-      originalKey: "Am",
+      title: "2. 這是耶和華所定的日子",
+      originalKey: "D",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[Am]以色列的[Dm]聖者 [G]為我犧牲自[C]己 [E7]\n[Am]神羔羊你[Dm]是彌賽亞 [F]耶穌[E7]和平之[Am]君\n[Am]我要跪下來[Dm]敬拜你 [G]主啊因為你是[C]偉大神 [E7]\n[Am]我要跪下來[Dm]敬拜你 [F]你是我[E7]的神我的[Am]王"
+      content: "[D]這是耶和[G]華 [D]這是耶和[G]華\n[D]所定的日[A7]子 [D]所定的日[A7]子\n[D]我們在[G]其中 [D]我們在[G]其中\n[D]要高興歡[A7]喜 [D]要高興歡[A7]喜\n[G]這是耶和華所[D]定的日子 [G]我們在其中要[D]高興歡喜\n[D]這是耶和[G]華 [D]這是耶和[G]華 [A7]所定的日[D]子"
     },
     {
-      title: "3. 獻上尊榮",
-      originalKey: "Am",
+      title: "3. 歡欣",
+      originalKey: "D",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "我們[Am]獻上尊榮[Dm]讚美 [G]在你的聖所[C]裡 [E7]\n我們[Am]獻上尊榮[Dm]讚美 [E7]在你的聖所[Am]裡\n[Am]我們歡呼 [Dm]我們讚美 [G]為著你的大慈[C]愛 [E7]\n[Am]我們歡呼 [Dm]我們讚美 [E7]為著你的大慈[Am]愛"
+      content: "[D]歡欣 感謝[F#m]主 歡欣 [G]因神愛子[D]耶穌基督\n[Bm]歡欣 感謝[F#m]主 歡欣 [G]因神愛子[D]耶穌基督\n[F#m]如今 [Bm]軟弱者能[Em]變剛強\n[A7]貧窮者成[D]為富足 [Bm]都因為主[Em]已成就了[A7]大事\n[D]歡欣 [D]歡欣"
     },
     {
-      title: "4. 歡呼收割",
-      originalKey: "Am",
+      title: "4. 常常喜樂",
+      originalKey: "D",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[Am]流淚撒種的 [Dm]必歡呼收割 [G]流淚撒種的 [C]必歡呼收割 [E7]\n[Am]那帶種流淚[Dm]出去的 [E7]必要歡呼地[Am]回來\n[Am]哈利路亞 [Dm]哈利路亞 [G]必要歡呼地[C]回來 [E7]\n[Am]哈利路亞 [Dm]哈利路亞 [E7]必要歡呼地[Am]回來"
+      content: "[D]常常喜[F#m]樂 向主[G]高歌 [A7]不論環[D]境如何\n[D]高山或[F#m]低谷主[G]都看顧 [A7]相信就[D]能面對\n[G]主會帶[A7]領 [F#m]主會保[Bm]守 [Em]主會賜下[A7]平安喜[D]樂"
     },
     {
-      title: "5. 擊鼓跳舞讚美祂",
-      originalKey: "Am",
+      title: "5. 野地的花",
+      originalKey: "D",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[Am]擊鼓跳舞 [G]讚美祂 [F]擊鼓跳舞 [E7]讚美祂\n[Am]擊鼓跳舞 [G]讚美祂 [F]讚美[E7]讚美[Am]祂\n[C]哈利路亞 [G]哈利路亞 [F]哈利路亞 [E7]讚美祂\n[Am]哈利路亞 [G]哈利路亞 [F]讚美[E7]讚美[Am]祂"
+      content: "[D]野地的[F#m]花 [G]穿著美[D]麗的衣裳\n[G]天空的[D]鳥兒 [E7]從來不為生活[A7]忙\n[D]慈愛的天[F#m]父 [G]天天都[D]看顧\n[G]祂更愛世[D]上人 [E7]為他們[A7]預備永[D]生"
     },
     {
-      title: "6. 我們爭戰的兵器",
-      originalKey: "Am",
+      title: "6. 尋找",
+      originalKey: "D",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[Am]我們爭戰的[G]兵器 [F]本不是屬血[E7]氣的\n[Am]乃是在神面前[G]有能力 [F]可以攻破堅固[E7]的營壘\n[C]將各樣的[G]計謀 [F]各樣攔阻人認識[E7]神的\n[Am]那些自高[G]之事 [F]一概[E7]攻破[Am]了"
+      content: "[D]尋找 尋[F#m]找 尋[G]找 真[D]愛\n[G]就在這[D]裡 [E7]就在這[A7]裡\n[D]耶穌的[F#m]愛 [G]是真[D]愛\n[G]尋找就[D]尋見 [A7]叩門就開[D]門"
     },
     {
-      title: "7. 神的子民",
-      originalKey: "Am",
+      title: "7. 有一天",
+      originalKey: "D",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[Am]神的子民 [G]大聲歡呼 [F]因為耶和華[E7]已得勝\n[Am]神的子民 [G]大聲歡呼 [F]因為[E7]主已得[Am]勝\n[C]高舉雙手 [G]讚美主名 [F]萬膝都要[E7]跪拜\n[Am]萬口都要 [G]承認耶穌 [F]祂是[E7]萬主之[Am]王"
+      content: "[D]有一天 [F#m]你若覺[G]得失去勇[D]氣\n[G]有一天 [D]你若真[E7]的想放[A7]棄\n[D]有一天 [F#m]你若感[G]覺沒人愛[D]你\n[G]有一天 [D]好像走[A7]到谷[D]底\n[G]那一天 [A7]你要振作[F#m]你的心[Bm]情\n[Em]那一天 [A7]你要珍惜[D]你自己\n[G]那一天 [A7]不要忘記[F#m]有人愛[Bm]你\n[Em]那一天 [A7]不要輕易[D]說放棄"
     },
     {
-      title: "8. 興起發光",
-      originalKey: "Am",
+      title: "8. 凡事都能做",
+      originalKey: "D",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[Am]興起發光 [G]興起發光 [F]因為你的光[E7]已經來到\n[Am]興起發光 [G]興起發光 [F]耶和華的[E7]榮耀[Am]發現\n[C]看哪黑暗 [G]遮蓋大地 [F]幽暗遮蓋[E7]萬民\n[Am]耶和華卻要 [G]顯現照耀你 [F]祂的榮耀[E7]要發現[Am]在你身上"
+      content: "[D]靠著那[F#m]加給我力量[G]的 主耶[D]穌\n[G]凡事我[D]都能做 [E7]凡事我都[A7]能做\n[D]靠著那[F#m]加給我力量[G]的 主耶[D]穌\n[G]凡事我[D]都能做 [A7]凡事我都[D]能做"
     },
     {
-      title: "9. 稱頌主聖名",
-      originalKey: "Am",
+      title: "9. 感謝神",
+      originalKey: "D",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[Am]主耶和華我的神[G]我的王 [F]我要世世代代稱頌[E7]你的名\n[Am]主耶和華我的神[G]我的王 [F]我要[E7]稱頌你的[Am]名\n[C]你是偉大 [G]配受大讚美 [F]你的大能誰能[E7]測度\n[Am]我要傳揚 [G]你的作為 [F]直到[E7]萬代萬[Am]世"
+      content: "[D]感謝神賜[G]路旁玫瑰 [A7]感謝神賜[D]香火[A7]微\n[D]感謝神賜[G]溫暖家庭 [A7]感謝神賜[D]福無量\n[G]感謝神賜[D]喜樂憂愁 [E7]感謝神賜[A7]屬天平安\n[D]感謝神賜[G]明天盼望 [A7]要感謝直[D]到永遠"
     },
     {
-      title: "10. 萬軍之耶和華",
-      originalKey: "Am",
-      timeSignature: "4/4",
+      title: "10. 奇異恩典",
+      originalKey: "D",
+      timeSignature: "3/4",
       editor: "烏鴉Lin",
-      content: "[Am]萬軍之耶[G]和華 [F]我的神[E7]我的王\n[Am]倚靠你的[G]人 [F]便[E7]為有[Am]福\n[C]在你祭壇[G]那裡 [F]麻雀為自己[E7]找著房屋\n[Am]燕子為自己[G]找著抱雛之窩 [F]住在你[E7]殿中的便為有[Am]福"
+      content: "[D]奇異恩[D7]典 何[G]等甘[D]甜\n[D]我罪已[Bm]得赦[E7]免 [A7]\n[D]前我失[D7]喪 今[G]被尋[D]回\n[D]瞎眼今[A7]得看[D]見"
     }
   ];
