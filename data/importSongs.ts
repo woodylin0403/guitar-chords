08b013521b87ccec0b0ec09aa00ff09eac51eb33
woodylin0403 <coolcrow0403@gmail.com>
2026-03-09 22:08:24 +0800

    song29

--- data/importSongs.ts
+++ data/importSongs.ts
@@ -1,65 +1,79 @@
 export const importSongs = [
   {
-    title: "38. 主內滿足",
-    originalKey: "F",
+    title: "1. 就是我",
+    originalKey: "Dm",
     timeSignature: "4/4",
     editor: "烏鴉Lin",
-    content: "(一)[F]救主耶穌 [C7]我一思念 [F]心中滿有甜味\n(二)[F]口所能唱 [C7]心所能想 [F]任何追憶思念\n(三)[F]我常思想 [C7]憂傷之人 [F]如何蒙主安慰\n(四)[F]惟主是我 [C7]平安福樂 [F]又是將來賞賜\n(一)[F]若是在天親見主面 [C7]必更香甜百[F]倍\n(二)[F]絕無一樣聲音能比 [C7]我主美名更[F]甜\n(三)[F]迷路之人如何救回 [C7]不禁感激滿[F]懷\n(四)[F]主的恩愛筆舌難述 [C7]深願你親嚐[F]受\n副歌 [F]惟主能使我心知足 [C7]主外別名無味\n[F]仁愛 生命 [Bb]永[Bb/F]遠喜樂 [C]全在[F]主耶穌內"
+    content: "1.[Dm]就是我 那[A7]就是我 [Dm]終日摸索[F7]歲月[Bb][C7]\n2.就是我 那就是我 暗淡人生路途\n1.[F]已蹉[Dm]跎 自從[Gm]來就主 [C7]甘願獻於[Dm]主\n2.感徬徨 所遇非我願 使我感疲倦\n[Gm7]歡欣[Am7]快樂臨到 [Dm]我\n換來只是爭戰一場\n副 [Bb]生命[C7]曾無意[F]義 [Dm]從未[Gm]有安[A7]息[Dm][D7]\n[Gm]但[A7]如今[Dm]全然改變 [Bb7]對生[Dm]命歡欣[Gm7]樂無比[A]"
   },
   {
-    title: "39. 那釘痕的手",
-    originalKey: "F",
+    title: "2. 平安 SHALOM",
+    originalKey: "Dm",
     timeSignature: "4/4",
     editor: "烏鴉Lin",
-    content: "1.(一)[F]在生命風暴中你曾否失敗 [C7]交托在那釘痕手[F]中\n2.(二)[F]你是否獨自行在幽暗陰影 [C7]交托在那釘痕手[F]中\n3.(三)[F]你心靈是否被罪重擔滿載 [C7]交托在那釘痕手[F]中\n1.(一)[F]是否疲乏勞苦都隨掙扎來 [C7]交托在那釘痕手[F]中\n2.(二)[F]若信靠主基督祂必慰你心 [C7]交托在那釘痕手[F]中\n3.(三)[F]快敞開你心門讓救主進來 [C7]交托在那釘痕手[F]中\n副歌 [Bb]交托在那釘痕手[F]中 [C7]交托在那釘痕手[F]中\n[Bb]祂是最好朋友 [F]始終要保守 [C7]交托在那釘痕手[F]中"
+    content: "[Dm]主耶穌你賜我平安 主耶穌你[Bb]賜我生[C]命\n[A]主耶穌你賜我喜樂 [Bb]主耶穌 Sha[A]lom\nSha[Dm]lom 是我的所有"
   },
   {
-    title: "40. 離了祂",
-    originalKey: "F",
-    timeSignature: "3/4",
-    editor: "烏鴉Lin",
-    content: "1.[F]離了祂我不能[Dm]作何事 [Gm]離了祂[C7]我定失[F]敗\n2.[F]離了祂生命誠[Dm]無意義 [Gm]離了祂[C7]罪轄制[F]我\n1.[Am]離了祂我飄流[F7]無依 [Bb]正如船 [C7]失去了[F]舵\n2.[Am]離了祂生活將[F7]空虛 [Bb]但靠耶穌[C7]我便得[F]救\n副歌 [F]耶穌 [Bb]哦 耶[F]穌 [C7]你是否認[F]識祂\n[F]你豈可錯過[Bb]祂 [F]哦 耶穌 [Dm]哦 耶[Gm]穌\n[F]離了祂[C7]我將會迷[F]失"
-  },
-  {
-    title: "41. 為你 為你",
-    originalKey: "F",
+    title: "3. 我們的心中多快樂",
+    originalKey: "Dm",
     timeSignature: "4/4",
     editor: "烏鴉Lin",
-    content: "[F]在[Bb]你心[F]靈 [Bb]一[Am7]切憂[Dm7]慮 [G]主耶穌已聽[C]見\n[G7]祂[C]都知[C7]道 [F]展[Bb]開雙[Am7]手 [Dm7]充[G]滿慈[G7]愛 [C] [C7] [Gm7]\n耶[F]穌正等[F/A]待你 [Bb]投靠依[F]賴 [Gm7]莫[C7]等待明[F]天 [C7]\n現[F]在接[Am7]受救[Dm7]主 [Gm7]在耶[Dm7]穌的[G7]手中 [C]必得享安[F]息\n[C7]耶[F]穌愛[Bb]你 [Am7]死在[Dm7]十架 [Gm7]獻上寶貴生[F]命\n[C]完[C7]全為[F]你 [Gm/F] [F]"
+    content: "[Dm]我們的心[Gm]中多快[Dm]樂 不停的唱那美妙的[Gm]歌\n[Am]耶穌愛[Dm]你 也[Gm]愛[Bb]我 我們的心連[Dm]成一顆\n[Bb]愛的[C]歌 和[Dm]平的[Bb]歌 燃[C]燒起不[Am]滅的[Dm]火 我們\n[Gm]舉起[Bb]燃燒的[Am]火 [Gm]插遍[Am]每個角[Dm]落"
   },
   {
-    title: "42. 你當以全心愛主你神",
-    originalKey: "F",
+    title: "4. 建造耶路撒冷",
+    originalKey: "Dm",
     timeSignature: "4/4",
     editor: "烏鴉Lin",
-    content: "[F]你當以全心愛主你神 [Gm7]你當盡全意[C7]愛主你神 [Gm7/Bb] [F] [Bb]\n[F]你當用悟性愛主你神 [Dm7]以你全心 [Gm7]盡你全意 [Gm7/C]用你 [C7]悟性\n(副)[Bb/C]哦主我[C7]愛你 [F/A]因你即[Bb/D]是萬有 [C/E]哦主我[F]愛你 [Dm7]因你成[Gm]就萬[Gm7/F]事\n[C/E]哦主我[C]愛你 [Gm7]因你成[C7]就萬事 [F]哦主我 [F/A]愛你 [Bb] [F/A] [Gm7]\n[Gm7/C]助我更[C7]愛你 [Bb/C]以我全[C/F]心盡我全意用我 [F]悟性"
+    content: "[Dm]主正在建[Gm]造耶路撒冷 主正在[A]建造\n耶路撒[Dm]冷 我主[D]正在聚[Gm]集 流亡的\n[C]以色列人 醫好傷[Dm]心人 裹他們的傷\n[D]主正在建[Gm]造 主正[A]在建[A7]造 耶路[Dm]撒冷"
   },
   {
-    title: "43. 更多有你",
-    originalKey: "F",
+    title: "5. 良人帶我",
+    originalKey: "Dm",
     timeSignature: "4/4",
     editor: "烏鴉Lin",
-    content: "[F]耶穌我心乾 [F/G]渴 [Gm7]懇[Gm7/C]求你[Bb/F]來充[F]滿[F/A]我\n[Bb]世上[C/Bb]事 [Am7]使我[Dm7]枯竭 [C/Bb]惟有[C/Bb]你能 [Am7]滿足\n[Dm7]我 我[Gm7]只願更[Bb/C]多 [C]有你我只願更[F/A]多 [Bb]有你 [C/E]\n[F]我只願[Bb/D]更多有[C/E]你我心別無所[F/A]求 [Bb/C] [C]\n[Dm7]求 惟有你主 [Eb] [Gm7/C]我只願更[F/A]多 [Bb] [C/E] [F]\n我只願[Bb/D]更多有[C]你我心[F/A]別無所 [Bb]求 [C] [Dm7]惟有[Gm]你主 [Gm7/C]惟有[F]你主"
+    content: "[Dm]良人帶我[Gm] [Am]進入[Dm]內室 [Bb]知心的[C]話兒說不[F]完 [Em]\n[Dm]我們的愛[Am]情 如[Gm]死之[Dm]堅強 [Gm]大水也[Am]不能淹[C]沒\n[Dm]我的良[Gm]人 白[Am]而且[Dm]紅 [Gm]超乎那[Am]萬人之[Gm]上 [C]\n[Gm]良人屬[Am]我 我[Dm]屬良[Gm]人 從[Bb]此也[C]不會離[F]分 [Em]\n[Dm]良人帶[Gm]我 往[Am]田間[Dm]去 [Gm]也在那[Am]葡萄園[Gm]中\n[Am]在百合[Dm]花中 [Am]牧放[Dm]群羊 我[Am]和良人[Gm]永 遠[Bb]\n同[F]行"
   },
   {
-    title: "44. 讚美耶和華的聖名",
-    originalKey: "F",
+    title: "6. 當向耶和華歡呼歌唱",
+    originalKey: "Dm",
     timeSignature: "4/4",
     editor: "烏鴉Lin",
-    content: "[F]讚美耶和華的[Bb/F]聖名 [C/F]讚美耶和華的聖名\n[F]讚美耶和華的[F/A]聖名 [Bb]至高[Gm7]主 [C]\n[F]榮耀耶和華的[Bb/F]聖名 [C/F]榮耀耶和華的聖名\n[F]榮耀耶和華的[F/A]聖名 [Bb]至高[Gm7]主 [C] [F]\n[Bb/F]主聖名被稱[F]為 堅固 堡[Bb/F]壘 [Gm/F]那 [F]\n為義的進入[C]它 必得永生 [Bb/C]主聖名被稱[F]為\n[Bb/F]堅固 堡[Gm/F]壘 [F]那為義的進入[C]它 必[Gm/C]得 [F/C]永[C7]生 [F]"
+    content: "[Dm]當向耶和華歡呼 當向耶和華歡呼 [Gm]當\n樂意事奉[Dm]祂 當來向祂歌唱 當曉得[A7]\n耶和華是[Dm]神[D7] 我們是祂[Gm]造的 也是屬於\n祂[A7]的 是祂的[Dm]民 草場的[Dm]羊 當稱謝[D7]讚美\n進祂[Dm]門 感謝稱頌[D7]祂 因為祂本為善\n[Gm]永遠慈愛 [A](一)祂信實直到萬[A]代\n[Dm] (二)祂信實[A7]直到萬[Dm]代"
   },
   {
-    title: "45. 我愛你主",
-    originalKey: "F",
+    title: "7. 向主唱新歌",
+    originalKey: "Dm",
     timeSignature: "4/4",
     editor: "烏鴉Lin",
-    content: "1.[C]我愛你[F]主 超越[Gm]一切 [C]我愛你主 全心\n[Bb]充滿感[F]謝 我愛你[Dm]主 多過[Am]愛自[Dm]己 我\n[Gm]愛你主 [C]我愛你主 [F]我愛你主\n2.[C]我敬拜你[F]主 你是我[Gm]唯一 [C]我敬拜你 將\n[Bb]我心獻給[F]你 我敬拜[Dm]你 我要[Am]永遠宣[Dm]揚\n[Gm]我敬拜你 [C]我敬拜你 [F]我敬拜你\n轉G調\n3.[D]我高舉[G]手 將生命[Am]獻給你 [D]我高舉手 全\n[C]心全意獻[G]祭 我高舉[Em]手 我的一[Bm]切屬於\n[Am]你 [D]我高舉手 [G]我高舉手 我高舉手\n轉A調\n4.[E]哈利路[A]亞 這是我[Bm]的頌讚 [E]哈利路亞 盡\n[D]我全心歌[A]唱 哈利路[F#m]亞 我的一[C#m]切屬於\n[Bm]你 [E]哈利路亞 [A]哈利路亞 哈利路亞"
+    content: "[Dm]向我們的主唱新歌 全地都來向祂歌[A7]唱\n[G]向我們的主[A7]唱新[A]歌 全地[A7]都來向祂歌[Dm]唱\n[Gm]神本為[Dm]大 當[Gm]受極大[Dm]讚美 [Gm]神為[Dm]大 當[Gm]受\n極[A]大讚美 [A7]喔"
   },
   {
-    title: "46. 我真愛進入你同在",
-    originalKey: "F",
+    title: "8. 向耶和華唱新歌",
+    originalKey: "Dm",
     timeSignature: "4/4",
     editor: "烏鴉Lin",
-    content: "[F]我真愛進入你[Bb]同在 與[F]你百姓齊聲\n[F/C]讚美 [C]我[F]要站立 要[Eb/F]喜樂 [Bb]舉雙手\n[C]揚聲讚[F/Bb]美 [F]我真愛[Bb]進入你[F]同在\n與[F/C]你百姓齊聲[C]讚美 [F]我要站立 [Eb/F]要喜樂 [Bb]舉\n雙手 揚聲[F/C]讚美 你[C]使我雙腳跳[F]舞 [Bb]以詩歌充[F]滿\n我 你是[Eb/Bb]我喜樂的[Bb]力量 [C7]喜樂\n[F]我真愛進入你[Bb]同在 與[F]你百姓\n齊聲[F/C]讚美 [C]我[F]要站立 要[Eb/F]喜樂 [Bb]舉\n雙手 [F/C]揚聲讚美 [C]舉雙[F]手 揚聲[Bb/F]讚[F/C]美\n舉[Gm7]雙手 [Bb/C]揚聲讚[F]美"
+    content: "[Am]你們要向耶[Dm]和華唱新歌 [F]全地都要向耶和華歌[Dm]唱\n[Gm7]要向耶[Gm]和華歌[Am]唱 稱頌祂的[Dm]名 祂的[Dm]名 天天傳揚\n[Gm]祂的救[C]恩 天天[Dm]傳揚祂的救恩 在列邦中述說[Gm7]\n[Dm]祂的榮[C]耀 在萬民[Dm]中述說祂的奇事 因耶和華[Gm7]\n[Gm]為大 當[Am]受極大的讚美 [Gm7]祂在[Gm]萬神之[Am]上 當[C7]受敬[Dm]畏"
+  },
+  {
+    title: "9. 心願",
+    originalKey: "Dm",
+    timeSignature: "4/4",
+    editor: "烏鴉Lin",
+    content: "[Dm]主啊 我願奉獻我自己 [Gm]主啊 我願永遠\n服事[Dm]你 無論我在那裡 無論[Gm]我何境[Dm]遇\n我願[Am]一生順服你旨[Dm]意 (男)願為主\n(女:願為[Gm]主)捨生[Dm]命 願為主作精兵\n[Gm]願為主作僕人 願為主服事[Dm]人 主啊\n[Dm]我願奉獻我自己 主[Gm]啊 我願永遠服事[Dm]你\n無論[Gm]我在那裡 無論我何境[Dm]遇 我願一生\n[Am]順服你旨[Dm]意 無論我在那裡 無論我何\n[Dm]境遇 我願[Am]一生順服你旨[Dm]意"
+  },
+  {
+    title: "10. 頌揚讚美神",
+    originalKey: "Dm",
+    timeSignature: "4/4",
+    editor: "烏鴉Lin",
+    content: "1.[Dm]蒙恩的人蒙恩的人 [E]頌 [A]揚 [Gm]讚 美 [A7]神 [Dm]\n2.基督是主基督是主 頌 揚 讚 美 神\n3.哈利路亞哈利路亞 頌 揚 讚 美 神\n1.[Gm]蒙恩的人蒙恩的人 [Dm]頌揚 [C]讚 美 [C7]神 [F]\n2.基督是主基督是主 頌揚 讚 美 神\n3.[Gm]哈利路亞哈利路亞 [Dm]頌揚 [G]讚 美 [A7]神 [Dm]"
+  },
+  {
+    title: "11. 唱哈利路亞",
+    originalKey: "Dm",
+    timeSignature: "4/4",
+    editor: "烏鴉Lin",
+    content: "[Dm]讓我們[Gm]來向[Dm]主耶和華歌唱 向[Bb]拯救我們的磐[C]石\n[Asus4]揚聲來歡[A]呼 [Dm]我們要以[Gm]感謝 [Dm]進入祂面前\n[Bb]讓我們[C]唱詩歌頌[Dm]祂聖名 [Gm]唱哈利路亞 [Dm]唱\n[Bb]哈利路亞 [C]唱[Asus4]哈利路[A]亞 [Dm]唱哈利路[Gm]亞 [Dm]唱哈利路[Bb]亞 [C]\n[Dm]路亞 [Dm]來讓我們跪下[Dm]在祂面前 俯伏敬[Bb]\n[C]拜創造我[Asus4]們的主[A]宰 [Dm]祂是我[Gm]們的神 [Dm]我們\n[Bb]都屬於[C]祂 是祂的[Dm]子民 是祂 所帶領的羊 唱\n[Gm]哈利路[Dm]亞 唱[Bb]哈利路[C]亞 [Asus4]唱哈利路[A]亞 [Dm]唱哈利路[Gm]亞 [Dm]唱哈利路[Bb]亞 [C]\n[Dm]路亞 唱[Am]哈利路[Dm]亞"
   }
 ];
