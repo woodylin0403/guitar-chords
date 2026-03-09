f55a8d0ceec8c467c798b253044dbf0e50d600c8
woodylin0403 <coolcrow0403@gmail.com>
2026-03-09 12:15:29 +0800

    song11

--- data/importSongs.ts
+++ data/importSongs.ts
@@ -1,114 +1,86 @@
 export const importSongs = [
     {
-      title: "1. 普天下向耶和華歡呼",
+      title: "1. 你是榮耀君王",
       originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[E]普天下向耶和[A]華歡[B]呼 [E]當歡喜事[A]奉耶和[B]華\n[E]當稱謝進入[A]祂的[B]門 [E]當讚美進入[A]祂的[B]院\n[A]當感謝祂 [B]稱頌祂的名 [A]當感謝祂 [B]稱頌祂的[E]名"
+      content: "[E] [B7] [E] [A] [B7]\n你是榮耀的君王你是和平之主\n[G#7] [C#m7] [A]\n你是天地萬有主宰 惟有你聖潔\n[B7] [G#7] [C#m7] [A]\n公義 天使都向你跪下敬拜尊\n[B7] [E] [A] [B7][C#m7][A]\n崇你因為在你有永生之道你\n[B7] [E]\n是主耶穌基督和散那歸於大衛\n[C#m] [A] [B7][E]\n的子孫和散那歸於萬王之王願\n[B7][E] [C#m7][A] [B7] [E]\n榮耀歸於至高神 耶穌是彌賽亞"
     },
     {
-      title: "2. 主我敬拜你",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]主我敬拜[A]你 [F#m]來到你座[B7]前\n[E]獻上我讚[A]美 [F#m]獻上我感[B7]謝\n[E]主我敬拜[A]你 [F#m]來到你座[B7]前\n[A]獻上我所[B7]有的[E]心"
-    },
-    {
-      title: "3. 耶和華作王",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]耶和華作王 [A]祂以威嚴為[B]衣 [E]耶和華作王 [A]祂已束上[B]能力\n[E]祂必作王[A]到永[B]遠 [E]祂必作王[A]到永[E]遠"
-    },
-    {
-      title: "4. 我的神我要讚美你",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]我的神我要[A]讚美[B]你 [E]我的神我要[A]讚美[B]你\n[E]你是我的[A]力量 [F#m]你是我的[B]盾牌\n[A]我要一生[B]稱謝[E]你"
-    },
-    {
-      title: "5. 聖潔羔羊",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]聖潔羔羊 [A]配得讚[B]美 [E]聖潔羔羊 [A]配得頌[B]揚\n[E]萬膝跪拜 [A]萬口承[B]認 [A]耶穌基督[B]是[E]主"
-    },
-    {
-      title: "6. 坐在寶座上聖潔羔羊",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]坐在寶座上[A]聖潔羔[B]羊 [E]願榮耀歸給[A]你直到[B]永遠\n[E]萬民都敬拜[A]你 稱頌[B]你 [A]配得榮耀[B]尊[E]貴"
-    },
-    {
-      title: "7. 讚美耶穌名",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]讚美耶穌[A]名 [B]讚美耶穌[E]名 [E]讚美耶穌[A]名 [B]哈利路[E]亞\n[E]祂是我的[A]主 [B]祂是我的[E]王 [E]祂是我的[A]救[B]主耶[E]穌"
-    },
-    {
-      title: "8. 聖哉聖哉聖哉",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]聖哉聖哉[A]聖[E]哉 [B]萬軍之耶和[E]華\n[E]聖哉聖哉[A]聖[E]哉 [B]全地充滿榮[E]耀"
-    },
-    {
-      title: "9. 哈利路亞主我神",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]哈利路亞[A]主我[E]神 [B]哈利路亞[E]主我神\n[E]哈利路亞[A]主我[E]神 [B]讚美我的[E]主"
-    },
-    {
-      title: "10. 我們要讚美耶和華",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]我們要讚美[A]耶和[B]華 [E]我們要讚美[A]耶和[B]華\n[E]讚美祂大能[A]的作為 [B]讚美祂極美[E]的大德"
-    },
-    {
-      title: "11. 讚美耶和華名",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]讚美耶和[A]華[B]名 [E]讚美耶和[A]華[B]名\n[E]從日出之地[A]到日落[B]之處 [A]主名配受[B]讚[E]美"
-    },
-    {
-      title: "12. 願主的名被高舉",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]願主的名[A]被高[B]舉 [E]願主的名[A]被高[B]舉\n[E]高舉在全地[A]之[B]上 [A]願主的名[B]被高[E]舉"
-    },
-    {
-      title: "13. 主耶穌我愛你",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]主耶穌我[A]愛[E]你 [B]主耶穌我[E]愛你\n[E]我的一生[A]全屬[E]你 [B]主耶穌我[E]愛你"
-    },
-    {
-      title: "14. 在祂沒有難成的事",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]在祂沒有[A]難成[B]的事 [E]在祂沒有[A]難成[B]的事\n[E]全能的上帝 [A]耶和[B]華 [A]在祂沒有[B]難成的[E]事"
-    },
-    {
-      title: "15. 哈利路亞讚美耶穌",
-      originalKey: "E",
-      timeSignature: "4/4",
-      editor: "烏鴉Lin",
-      content: "[E]哈利路亞[A]讚美[E]耶穌 [B]哈利路亞[E]讚美耶穌\n[E]哈利路亞[A]讚美[E]耶穌 [B]讚美祂[E]聖名"
-    },
-    {
-      title: "16. 願主賜福你",
+      title: "2. 求充滿我",
       originalKey: "E",
       timeSignature: "3/4",
       editor: "烏鴉Lin",
-      content: "[E]願主賜福[A]給你 [B]願主保護[E]著你\n[E]願主使祂臉[A]光照[E]你 [B]賜你[E]平安"
+      content: "[E][E7][A] [E] [C#m] [F#m7] [B7]\n充滿我主聖靈\n[E] [E7] [A]\n充滿我主聖靈\n解我乾渴的靈\n[F#m7][B7] [E]\n求使我得完全"
+    },
+    {
+      title: "3. 我們高舉雙手",
+      originalKey: "E",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[E] [E7] [A]\n我們高舉雙手來讚美主 我們抬\n[E]\n頭仰望來讚美主凡被造的萬物\n[B7] [E] [A]\n都來讚美主 萬王之王耶穌 萬\n[E] [B7] [E]\n主之主耶穌 賜我新生命讚美主\n[E7] [A]\n(副)哈利路亞來讚美主 哈利路亞來\n[E] [B7] [B7]\n讚美主賜我新生命 1.讚美主\n[E]\n2.讚美主"
+    },
+    {
+      title: "4. 父神我敬拜你",
+      originalKey: "E",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[E] [F#m] [B7][E] [A] [B7][E]\n父神我敬拜你我一生交託你\n[E] [F#m] [B7][E]\n我真愛你 (2)耶穌(3)聖靈"
+    },
+    {
+      title: "5. 祂已被尊崇",
+      originalKey: "E",
+      timeSignature: "6/8",
+      editor: "烏鴉Lin",
+      content: "[E] [E] [A]\n祂已被尊崇 祂名稱為至高君王\n[A] [B7] [E] [E]\n我要讚美祂祂以被尊崇 祂作\n[A] [B7][A] [B7][C#m] [F#m]\n王到永遠來讚美祂聖名祂\n[C#m] [B7] [E] [C#m] [A] [E] [F#m]\n已作王 祂真理永遠掌權天\n[C#m] [B7] [E] [G#m] [A] [E] [F#m]\n上地下歡呼頌揚祂聖名 祂已\n[D] [B7] [E]\n被尊崇 祂名稱為至高君王"
+    },
+    {
+      title: "6. 三一頌",
+      originalKey: "E",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[E] [G#m] [A] [E] [A] [E] [B7]\n普天之下萬國萬民 齊聲讚美父子\n[E] [E] [G#m] [A] [E] [A]\n聖靈三位一體同榮同尊 萬有之\n[E] [B7] [E]\n源萬福之本"
+    },
+    {
+      title: "7. 耶穌降世為你",
+      originalKey: "E",
+      timeSignature: "3/4",
+      editor: "烏鴉Lin",
+      content: "[E] [A] [B7] [G#m] [C#m] [A]\n耶穌降世為你耶穌降世為我耶\n[B7] [G#m] [C#m] [A] [B7] [E]\n穌降世為你為我 我願為祂而活"
+    },
+    {
+      title: "8. 神啊求你鑑察",
+      originalKey: "E",
+      timeSignature: "9/8",
+      editor: "烏鴉Lin",
+      content: "[E] [A] [B7] [E] [B7]\n神啊求你鑑察我知道我的心思試煉我知道我的意念看在我裡面有什麼惡\n[E] [E] [A] [E] [B7] [E]\n行沒有引導我走永生的道路"
+    },
+    {
+      title: "9. 惟有你",
+      originalKey: "E",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[E] [B7] [A] [E]\n惟有你惟有你惟有你是我所愛\n[A] [E] [F#m7] [A]\n惟有你哦主我王 我願能更認識\n[B7] [A] [B7][E] [E] [A] [E]\n你我全心我全人都向你深深敬\n[Bm7][E] [E] [B7] [G#m] [C#m] [A]\n拜聖潔聖潔 尊貴尊貴之主 我\n[E] [F#m][B7][E]\n愛你耶穌"
+    },
+    {
+      title: "10. 拯救我國",
+      originalKey: "E",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "1.[E] [A] [B7] [F#m] [B7]\n主我們懇求你大能運行 我們深深\n[E] [C#m] [G#m] [F#m]\n向你呼喊顯明醫治 在我國家差\n[E] [B7]\n遣聖靈來復興我們\n2.[A] [B7] [F#m] [B7]\n主我們已聽見 春雨響聲聖靈火浪\n[E] [C#m] [G#m]\n正湧向我們顯明公義 寬恕赦免\n[F#m] [E] [B7]\n懇求呼求來復興我們\n[E][G#M][A] [E] [G#m] [A] [F#m7] [B7] [E][A][E]\n(副)醫治我國 拯救我國 聖靈澆灌在這國家"
+    },
+    {
+      title: "11. 我們讚美你",
+      originalKey: "E",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[E] [F#m] [B7]\n我們在此讚美揚心高聲唱\n[E] [F#m] [B7] [E]\n我們向你獻上最美好的一切\n[E] [A] [B] [G#m]\n是我們的愛從心底流出\n[C#m] [F#m] [B7] [E]\n在我裏面要訴說 主我愛你\n[G#m] [C#m] [F#m] [B7] [F#m][E]\n我心我靈都訴說 主我愛你"
+    },
+    {
+      title: "12. 我們是你的百姓",
+      originalKey: "E",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[E] [F#m] [B7] [E] [F#m7] [B7]\n我們屬於你都是你的百姓我們正呼\n[E] [E7] [A]\n求你宣揚你的聖名 在這黑暗的世代\n[F#m] [E] [C#m] [F#m7] [B7]\n你召我們發光當我們尋求你面 主啊\n[E] [G#m7] [C#m7] [A] [Am]\n顯你大能建立你教會 來醫治這地\n[E] [F#m] [B7] [E] [G#m7] [C#m] [A] [Am]\n願你國度降臨 建立你教會來醫治這地"
     }
   ];
