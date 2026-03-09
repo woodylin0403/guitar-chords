04a29bcc641860b0ec8350289574f36c0ac88c26
woodylin0403 <coolcrow0403@gmail.com>
2026-03-09 10:44:18 +0800

    歌詞13

--- data/importSongs.ts
+++ data/importSongs.ts
@@ -1,72 +1,72 @@
 export const importSongs = [
     {
-      title: "61. 讓愛走動",
-      originalKey: "D",
+      title: "1. 一切全獻上",
+      originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[D]走過傷心 [F#m]走過淚水 [G]讓每一顆[E7]心 再次[A7]飛舞\n[D]走過失去 [F#m]走過破碎 [G]讓每一顆[A7]心 再次[D]起步\n[G]讓愛[A7]走動 [F#m]讓你的愛[Bm]化成希望 [G]走過[A7]世界每個[D]角落\n[G]讓愛[A7]走動 [F#m]讓你的愛[Bm]化成力量 [Em]帶給[A7]世界換然[D]一新"
+      content: "[E]為主我願[A]一切全獻上 [B]將我全心[E]交給祢\n[C#m]一生與祢[F#m]同行同經歷 [B]全心信靠[E]祢到底\n[E]一切全獻[C#m]上 [A]一切[B]全獻[E]上\n[C#m]恩主[F#m]我救主 [B]一切全獻[E]上"
     },
     {
-      title: "62. 耶和華神已掌權",
-      originalKey: "D",
+      title: "2. 我要愛慕祢",
+      originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[|] [D] [|] [A] [|] [Bm] [|] [F#m] [|]\n[D]主啊你[A]是 [Bm]我們的[F#m]神\n[G]全地都[D]要 [Em]來敬拜[A7]你\n[D]主啊你[A]是 [Bm]我們的[F#m]王\n[G]全地都[D]要 [A7]來讚美[D]你\n[G]耶和華[A7]神已[F#m]掌權 [Bm]\n[G]萬國萬[A7]民齊來[D]歡呼 [D7]\n[G]耶和華[A7]神已[F#m]掌權 [Bm]\n[Em]全地都[A7]要來讚美[D]你"
+      content: "[|] [E] [|] [G#m] [|] [C#m] [|] [G#m] [|]\n[E]耶穌 [G#m]我要愛慕祢 [C#m]一生 [G#m]愛慕祢\n[A]我的心 [E]單單渴慕祢 [F#m]我要尋求[B]祢面\n[E]耶穌 [G#m]我要愛慕祢 [C#m]一生 [G#m]愛慕祢\n[A]我的心 [E]單單渴慕祢 [F#m]我要[B]尋求祢[E]面\n[A]我要瞻仰祢[G#m]的榮美 [C#m] [A]等候在[B]祢跟[E]前\n[A]我要瞻仰祢[G#m]的榮美 [C#m] [F#m]等候[B]在祢跟[E]前"
     },
     {
-      title: "63. 我要歌頌祢聖名",
-      originalKey: "D",
+      title: "3. 深深愛祢",
+      originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[|] [D] [|] [G] [|] [D] [|] [A7] [|]\n[D]我要歌[F#m]頌你聖[Bm]名 哦[F#m]主\n[G]我要歌[D]頌你聖[E7]名 哦[A7]主\n[D]因為你[F#m]名甚是[Bm]奇妙 [G]配得[D]一切[A7]的讚[D]美\n[G]哈利路[A7]亞 [F#m]讚美[Bm]主 [Em]哈利路[A7]亞 讚美[D]主"
+      content: "[E]我生命[A]中最渴望[B]的一件[E]事\n[C#m]切慕祢 [F#m]單單尋[B]求祢\n[E]用我全[A]心全意 [B]用我全[E]力愛祢\n[C#m]敬拜祢 [F#m]讓祢榮[B]耀降[E]臨\n[G#m]深深愛[C#m]祢 [A]耶穌 [B]深深愛[E]祢 耶穌\n[C#m]我愛祢[F#m]超越生[A]命中 [B]的一切\n[G#m]深深愛[C#m]祢 [A]耶穌 [B]深深愛[E]祢 耶穌\n[C#m]哦我愛[F#m]祢耶[B]穌 [E]"
     },
     {
-      title: "64. 差遣我",
-      originalKey: "D",
+      title: "4. 阿爸天父",
+      originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[|] [D] [|] [A] [|] [Bm] [|] [F#m] [|]\n[D]主啊你[A]若願意 [Bm]差遣[F#m]我\n[G]我願為[D]你 去到[Em]地[A7]極\n[D]主啊你[A]若願意 [Bm]使用[F#m]我\n[G]我願一[A7]生 奉獻給[D]你\n[G]差遣我 [A7]差遣我 [F#m]到萬邦[Bm]萬民中\n[Em]宣揚你[A7]的奇妙救[D]恩 [D7]\n[G]差遣我 [A7]差遣我 [F#m]到未得之[Bm]民中\n[Em]傳揚你[A7]的榮耀主[D]名"
+      content: "[E]阿爸[A]天父 [B]阿爸天[E]父\n[C#m]從我心[F#m]深處 [B]呼求祢[E]名字\n[E]阿爸[A]天父 [B]阿爸天[E]父\n[C#m]從我心[F#m]深處 [B]呼求祢[E]名字\n[A]祢是我[B]的盾牌 [G#m]祢是我[C#m]的榮耀\n[F#m]又是叫我[B]抬起頭來[E]的[E7]\n[A]祢是我[B]的盾牌 [G#m]祢是我[C#m]的榮耀\n[F#m]又是叫我[B]抬起頭來[E]的"
     },
     {
-      title: "65. 將天敞開",
-      originalKey: "D",
+      title: "5. 這世代",
+      originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[|] [D] [|] [G] [|] [D] [|] [A7] [|]\n[D]將天敞開 [G]你的榮耀[D]降臨 [D]將天敞開 [E7]你的同在[A7]降臨\n[D]將天敞開 [G]你的大能[D]降臨 [G]我們歡呼 [A7]迎接你的[D]降臨\n[G]萬國萬[A7]民 [F#m]都要齊來[Bm]敬拜\n[G]列邦列[A7]國 [F#m]都要屈膝[Bm]敬拜\n[Em]我們歡[A7]呼 迎接你[D]的降臨"
+      content: "[|] [E] [|] [B] [|] [C#m] [|] [G#m] [|] [A] [|] [E] [|] [F#m] [|] [B] [|]\n[E]這世代[B]要呼求祢 [C#m]這世代[G#m]要尋求祢\n[A]這世代[E]要看見 [F#m]祢的大[B]能\n[E]這世代[B]要呼求祢 [C#m]這世代[G#m]要尋求祢\n[A]這世代[E]要看見 [F#m]祢的[B]大[E]能\n[A]我們要[B]舉起聖潔[G#m]的手[C#m] [F#m]向祢[B]呼[E]求[E7]\n[A]我們要[B]舉起聖潔[G#m]的手[C#m] [F#m]向祢[B]呼[E]求"
     },
     {
-      title: "66. 展開清晨的翅膀",
-      originalKey: "D",
+      title: "6. 主我敬拜祢",
+      originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[|] [D] [|] [A] [|] [Bm] [|] [F#m] [|]\n[D]主耶和[A]華 你已[Bm]鑒察了[F#m]我\n[G]我坐下 [D]我起來 [Em]你都已曉[A7]得\n[D]我行路 [A]我躺臥 [Bm]你都細察[F#m]過\n[G]我也深[D]知 你與[A7]我同[D]在\n[G]我若展[A7]開 清晨的[F#m]翅膀 [Bm]\n[G]飛到海[A7]極 去居[D]住 [D7]\n[G]就是在那[A7]裡 你的[F#m]手必引導[Bm]我\n[Em]你的右[A7]手 必扶持[D]我"
+      content: "[E]主我[A]敬拜祢 [B]敞開[E]我的心\n[C#m]無人[F#m]能與祢 [B]相[E]比\n[E]主我[A]敬拜祢 [B]敞開[E]我的心\n[C#m]無人[F#m]能與祢 [B]相[E]比\n[A]祢是[B]我的一切 [G#m]祢是[C#m]我的唯一\n[F#m]主我[B]敬拜[E]祢[E7]\n[A]祢是[B]我的一切 [G#m]祢是[C#m]我的唯一\n[F#m]主我[B]敬拜[E]祢"
     },
     {
-      title: "67. 和散那",
-      originalKey: "D",
+      title: "7. 我的神我的父我的磐石",
+      originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[|] [D] [|] [G] [|] [D] [|] [A7] [|]\n[D]和散那 [G]和散[D]那 [D]和散那歸於[E7]至高[A7]神\n[D]和散那 [G]和散[D]那 [D]和散那歸於[A7]至高[D]神\n[G]我們高舉[A7]你名 [F#m]心中充滿[Bm]讚美\n[G]齊來歡呼[A7]歌頌 [D]和散那歸於[A7]至高[D]神"
+      content: "[E]我的神[A]我的父 [B]我的磐[E]石\n[C#m]我敬拜[F#m]我讚美 [B]祢的聖[E]名\n[E]我的神[A]我的父 [B]我的磐[E]石\n[C#m]我敬拜[F#m]我讚美 [B]祢的聖[E]名\n[A]祢是[B]我的高臺 [G#m]祢是[C#m]我的避難所\n[F#m]我要[B]一生倚靠[E]祢[E7]\n[A]祢是[B]我的高臺 [G#m]祢是[C#m]我的避難所\n[F#m]我要[B]一生倚靠[E]祢"
     },
     {
-      title: "68. 讚美祭",
-      originalKey: "D",
+      title: "8. 我們呼求",
+      originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[|] [D] [|] [F#m] [|] [G] [|] [A7] [|]\n[D]我要獻上 [F#m]讚美祭 [G]我要高舉[A7]耶穌的[D]名\n[D]我要獻上 [F#m]讚美祭 [G]我要宣揚[A7]主大[D]能\n[G]因祂本為[A7]善 [F#m]祂的慈愛[Bm]永遠長存\n[Em]因祂本為[A7]善 [D]祂的信實直到[D7]萬代\n[G]我要獻上 [A7]讚美祭 [F#m]我要高舉[Bm]耶穌的名\n[Em]我要宣揚[A7]主大[D]能"
+      content: "[E]我們呼求[B]祢的名 [C#m]求祢恩典[G#m]降臨\n[A]我們呼求[E]祢的名 [F#m]求祢憐憫[B]降臨\n[E]我們呼求[B]祢的名 [C#m]求祢恩典[G#m]降臨\n[A]我們呼求[E]祢的名 [F#m]求祢[B]憐憫降[E]臨\n[A]醫治[B]這地 [G#m]醫治[C#m]我們\n[F#m]我們要[B]看見祢的[E]榮耀[E7]\n[A]醫治[B]這地 [G#m]醫治[C#m]我們\n[F#m]我們要[B]看見祢的[E]榮耀"
     },
     {
-      title: "69. 榮耀羔羊",
-      originalKey: "D",
+      title: "9. 全能的創造主",
+      originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[|] [D] [|] [A] [|] [Bm] [|] [F#m] [|]\n[D]榮耀 [A]榮耀 [Bm]榮耀神羔[F#m]羊\n[G]榮耀 [D]榮耀 [Em]榮耀神羔[A7]羊\n[D]唯有祂[A]是 榮[Bm]耀尊貴的[F#m]主\n[G]唯有祂[D]是 配[A7]得一切讚[D]美\n[G]齊來敬[A7]拜 [F#m]在祂寶座[Bm]前\n[Em]高舉雙[A7]手 讚美[D]祂"
+      content: "[E]全能的[A]創造主 [B]祢配得[E]讚美\n[C#m]全能的[F#m]創造主 [B]我們敬拜[E]祢\n[E]全能的[A]創造主 [B]祢配得[E]讚美\n[C#m]全能的[F#m]創造主 [B]我們敬拜[E]祢\n[A]天地[B]都要廢去 [G#m]祢的[C#m]話語不改變\n[F#m]祢的[B]慈愛到永[E]遠[E7]\n[A]天地[B]都要廢去 [G#m]祢的[C#m]話語不改變\n[F#m]祢的[B]慈愛到永[E]遠"
     },
     {
-      title: "70. 我們歡迎君王降臨",
-      originalKey: "D",
+      title: "10. 耶和華恩年已來到",
+      originalKey: "E",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[|] [D] [|] [G] [|] [D] [|] [A7] [|]\n[D]宇宙的中心 [G]耶穌 [D]世界的中心 [E7]耶[A7]穌\n[D]萬物都造 [G]為你 [D]萬物都歸[A7]於[D]你\n[G]我們歡[A7]迎 君王[F#m]降[Bm]臨\n[G]我們歡[A7]迎 君王[D]降[D7]臨\n[G]渴望你[A7]榮耀 顯現[F#m]在這[Bm]地\n[Em]我們歡[A7]迎 君王[D]降臨"
+      content: "[|] [E] [|] [A] [|] [E] [|] [B] [|]\n[E]耶和華[A]恩年已來[E]到\n[E]神的子[F#m]民齊來歡[B]呼\n[E]耶和華[A]恩年已來[E]到\n[E]神的子[B]民齊來歡[E]呼\n[A]瞎眼的[B]得看見 [G#m]瘸腿的[C#m]得行走\n[F#m]受壓制的[B]得自[E]由[E7]\n[A]瞎眼的[B]得看見 [G#m]瘸腿的[C#m]得行走\n[F#m]受壓制的[B]得自[E]由"
     }
   ];
