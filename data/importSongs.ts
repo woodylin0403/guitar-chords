3ef3028d205e5ea2533cd8b188d3eb267bb4dcd5
woodylin0403 <coolcrow0403@gmail.com>
2026-03-09 11:09:34 +0800

    歌詞19

--- data/importSongs.ts
+++ data/importSongs.ts
@@ -1,72 +1,72 @@
 export const importSongs = [
     {
-      title: "21. 何等恩友",
+      title: "31. 天父必看顧你",
       originalKey: "F",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[F]何等恩友[Bb]慈仁救主 [F]擔我罪孽[C]擔我憂\n[F]何等權利[Bb]能將萬事 [F]帶到主恩[C]座前[F]求\n[C]多少平安[F]屢屢失去 [Bb]多少痛苦[F]白白[C]受\n[F]皆因我們[Bb]未將萬事 [F]帶到主恩[C]座前[F]求\n[F]是否軟弱[Bb]勞苦多愁 [F]掛慮重擔[C]壓肩頭\n[F]主仍是我[Bb]避難處所 [F]帶到主恩[C]座前[F]求"
+      content: "[F]任遭何事不[Bb]要驚[F]怕 [C]天父必看顧[F]你\n[F]必將你藏祂[Bb]恩翅[F]下 [C]天父必看顧[F]你\n[F]天父必看顧[Bb]你 [C]時時看顧 [F]處處看顧\n[F]祂必要看顧[Bb]你 [C]天父必看顧[F]你"
     },
     {
-      title: "22. 信靠耶穌何其甘甜",
+      title: "32. 耶穌領我",
       originalKey: "F",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[F]信靠耶穌[Bb]何其甘甜 [F]抓著祂的[C]應許\n[F]信靠耶穌[Bb]何其甘甜 [F]相信主[C]的言[F]語\n[F]耶穌耶穌[Bb]何等可靠 [F]我已試過[C]多少次\n[F]耶穌耶穌[Bb]寶貴耶穌 [F]賜我恩[C]典更信[F]靠"
+      content: "[F]耶穌領我[Bb]我何安[F]慰 [Dm]這話滿有[G7]慈悲恩[C]惠\n[F]無論在何[Bb]地方境[F]況 [Bb]總有耶穌[C]親手引[F]領\n[F]主耶穌領我 [C]主耶穌領我 [F]主耶穌親[G7]手引領[C]我\n[F]我願如同[Bb]順服門[F]徒 [Bb]因有耶穌[C]親手引[F]領"
     },
     {
-      title: "23. 我時刻需要祢",
-      originalKey: "F",
-      timeSignature: "3/4",
-      editor: "烏鴉Lin",
-      content: "[F]我時刻需要祢 [Bb]恩主耶[F]穌\n[C]無人語[F]音像祢 [G7]能慰我[C]苦\n[F]求主施恩救我 [Bb]我時刻[F]需要祢\n[Bb]求主今來[F]賜福 [C]我來就[F]祢"
-    },
-    {
-      title: "24. 向高處行",
-      originalKey: "F",
-      timeSignature: "3/4",
-      editor: "烏鴉Lin",
-      content: "[F]我今直向[Bb]高處而行 [F]靈性地位[G7]日日高[C]昇\n[F]我行天路[Bb]有時軟弱 [F]求主扶持[C]進入光[F]明\n[C]求主扶持[F]使我堅立 [Bb]因信站在[F]高處之[C]地\n[F]使我站立[Bb]在更高處 [F]求主扶持[C]引我前[F]行"
-    },
-    {
-      title: "25. 堅固磐石",
-      originalKey: "F",
-      timeSignature: "3/4",
-      editor: "烏鴉Lin",
-      content: "[F]我心所望[C]別無根基 [F]惟有救主[C]流血公[F]義\n[F]除此以外[C]空虛無憑 [F]只靠耶穌[C]救主聖[F]名\n[F]立在基督[Bb]磐石堅固 [F]其餘根基[C]全是沙[F]土\n[F]其餘根基[C]全是沙[F]土"
-    },
-    {
-      title: "26. 寶貴泉源",
+      title: "33. 更加與主接近",
       originalKey: "F",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[F]有一血泉[Bb]血流盈滿 [F]湧自救主[C]肋旁\n[F]罪人只要[Bb]在此一洗 [F]能將罪[C]污洗[F]良\n[F]能將罪[Bb]污洗良 [F]能將罪[C]污洗良\n[F]罪人只要[Bb]在此一洗 [F]能將罪[C]污洗[F]良"
+      content: "[F]更加與主[Bb]接[F]近 [F]更加[C]接[F]近\n[F]縱然釘十[Bb]字[F]架 [F]高舉[C]我[F]身\n[F]我心依然[Bb]歌[F]唱 [F]更加與[Bb]主接[C]近\n[F]更加與主[Bb]接[F]近 [F]更加[C]接[F]近"
     },
     {
-      title: "27. 照我本相",
-      originalKey: "F",
-      timeSignature: "3/4",
-      editor: "烏鴉Lin",
-      content: "[F]照我本相[C]無善足稱 [F]惟祢流血[C]替我受[F]懲\n[Bb]並且呼召[F]就祢得生 [F]救主耶穌[C]我來我[F]來\n[F]照我本相[C]祢肯收留 [F]賜我生命[C]赦我愆[F]尤\n[Bb]祢既應許[F]必定成就 [F]救主耶穌[C]我來我[F]來"
-    },
-    {
-      title: "28. 只要信祂",
+      title: "34. 我心靈得安寧",
       originalKey: "F",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[F]凡勞苦擔[Bb]重擔的人 [F]耶穌親自[C]呼召\n[F]若肯信靠[Bb]必得安息 [F]只要[C]信靠[F]祂\n[F]只要信祂 [Bb]只要信祂 [F]現在[G7]信靠[C]祂\n[F]祂必救你 [Bb]祂必救你 [F]現在[C]拯救[F]你"
+      content: "[F]有時享平[Bb]安如[C]江河平[F]又穩\n[Dm]有時悲傷[G7]來似浪[C]滾\n[F]不論何環[Bb]境 [C]主已教導我[F]說\n[Bb]我心靈 [C]得安寧 [F]得安寧\n[F]我心[C]靈 [C]得安[F]寧\n[Bb]我心[F]靈 [C]得安[F]寧"
     },
     {
-      title: "29. 耶穌呼召",
+      title: "35. 快樂日",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F]快樂日 [Bb]快樂[F]日 [F]救主洗淨[C]我眾罪[F]孽\n[F]心裡歡喜 [Bb]快樂無[F]邊 [F]從今以後[C]靠主度[F]日\n[F]快樂日 [Bb]快樂[F]日 [F]救主洗淨[C]我眾罪[F]孽"
+    },
+    {
+      title: "36. 賜福的雨",
       originalKey: "F",
       timeSignature: "6/8",
       editor: "烏鴉Lin",
-      content: "[F]耶穌慈聲[Bb]懇切呼召 [F]呼召你[C]呼召我\n[F]祂在恩門[Bb]等候觀望 [F]等候你[C]等候[F]我\n[C]歸家 [F]歸家 [Bb]疲乏愁苦人[F]歸[C]家\n[F]耶穌慈聲[Bb]懇切呼召 [F]呼召罪[C]人歸[F]家"
+      content: "[F]真神應許[Bb]賜下恩[F]雨 [F]必從天上[G7]降下[C]來\n[F]聖靈豐滿[Bb]又賜安[F]息 [Bb]如同活水[C]流不[F]息\n[F]恩雨 [C]降下恩[F]雨 [F]求主現在[G7]降恩[C]雨\n[F]雖然已有[Bb]點滴降[F]下 [Bb]我們還要[C]大恩[F]雨"
     },
     {
-      title: "30. 每當我想起祢",
+      title: "37. 時刻蒙恩",
       originalKey: "F",
       timeSignature: "4/4",
       editor: "烏鴉Lin",
-      content: "[F]每當我想起祢 [C]祢的慈愛擁抱我 [Dm]我的心不再漸漸冷漠 [Am]\n[Bb]因祢愛光照我 [F]在我生命中 [Gm]祢賜下豐盛的恩典 [C]\n[F]我要讚美 [C]我要敬拜 [Dm]我的主我的神 [Am]\n[Bb]祢的愛是我 [F]生命的力量 [Gm]我要一生 [C]跟隨祢 [F]"
+      content: "[F]與基督同死[Bb]我已向罪[F]死\n[C]與基督同活[G7]我向神活[C]著\n[F]我今仰望主[Bb]無一人能[F]阻\n[Bb]因為主耶穌[C]時時看顧[F]我\n[C]時刻蒙保[F]守 [D]時刻蒙恩[Gm]典\n[C]時刻蒙引[F]導 [G7]無處不平[C]安\n[F]我今仰望主[Bb]無一人能[F]阻\n[Bb]因為主耶穌[C]時時看顧[F]我"
+    },
+    {
+      title: "38. 榮耀釋放",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F]我曾被罪[Bb]惡鎖鍊[F]所捆[C]綁\n[F]我曾被罪[Bb]惡捆綁[F]無自[C]由\n[F]但耶穌斷[Bb]開我一[F]切鎖[C]鍊\n[F]榮耀的釋[Bb]放我今[C]已獲[F]得\n[F]榮耀釋放 [Bb]奇妙釋[F]放\n[F]我主耶穌[G7]使我釋[C]放\n[F]恩典洋溢 [Bb]如江河[F]流淌\n[Bb]榮耀的釋[F]放我今[C]已獲[F]得"
+    },
+    {
+      title: "39. 谷中百合花",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F]主耶穌是我良[Bb]友有主[F]勝得萬有\n[F]萬人中救主是[G7]我最好靈[C]友\n[F]主是谷中百合[Bb]花我惟[F]一需要祂\n[Bb]祂能洗淨我使[C]我聖潔無[F]瑕\n[Bb]悲傷時祂來解憂 [F]患難時祂保全\n[F]一切顧慮全交[G7]托主恩[C]手\n[F]主是晨星燦爛[Bb]光華是[F]谷中百合花\n[Bb]萬人中救主是[C]我最好靈[F]友"
+    },
+    {
+      title: "40. 愛主更深",
+      originalKey: "F",
+      timeSignature: "4/4",
+      editor: "烏鴉Lin",
+      content: "[F]愛主更[Bb]深 [F]我主耶[C]穌\n[F]屈膝敬[Bb]拜 [F]懇求聽[C]我[F]祈\n[F]這是我[Bb]心所求\n[F]愛主更[Bb]深 [F]愛主更[C]深 [F]"
     }
   ];
