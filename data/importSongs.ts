7d2f65d6baa6b702b8cae7b9d10104d268e30f29
woodylin0403 <coolcrow0403@gmail.com>
2026-03-10 14:07:17 +0800

    newsong1

--- data/importSongs.ts
+++ data/importSongs.ts
@@ -1,23 +1,9 @@
 export const importSongs = [
   {
-    title: "34. 祂是我主我神",
-    originalKey: "A",
+    title: "親愛主聖靈",
+    originalKey: "D",
     timeSignature: "4/4",
     editor: "烏鴉Lin",
-    content: "[A]天上星宿月[F#m]亮祂設立 [Bm]御風擊電尊[E]貴[A][F#m]\n[Bm]有能力[E] [A]雖然是祂掌管天和地 [A7]於我[D]何[C#m]\n[Bm]意義[E7] [A] [F#m]基督降生普世大歡[Bm]欣[E][A]史\n[F#m]家稱為[Bm]斷代[E]大事記 祂[A]來釋放奴僕從[D]\n[C#m]罪裡 [A]於我[D]何意義 直到憑著信心見\n[D]主面 直到[C#m]享受奇妙大恩典 [Bm]我才知\n[D]道榮耀神[Bm]顧念 [D]卑微[E]渺小的[D]人 祂謙卑\n[E]眷[D]顧人[E7]祂 [A]扶我[F#m]伴我牽我[Bm]每一天[A]看\n[F#m]顧保守[Bm]使我[E]無掛[A]牽 [E]領我走窄[A]路到父[D]\n[Bm]神面前\n[A]1.祂是我[Bm]主我[A]神[A7] [A]2.祂是我[Bm]主[C#m]我神[A]"
-  },
-  {
-    title: "35. 神掌權",
-    originalKey: "A",
-    timeSignature: "4/4",
-    editor: "烏鴉Lin",
-    content: "[D]何等佳[E7]美 [A]那傳主福音的腳蹤 [Bm]傳揚[E7]佳音\n[A]佳[A7]音 [D]宣揚[E7]平安 [A]傳報真喜樂的信息 神\n[Bm]掌[E7]權 [A]神掌權 [D]神掌權 [A]神掌權 [D]神掌[E7]權\n[A]神掌權"
-  },
-  {
-    title: "36. 尊崇歸主我神",
-    originalKey: "A",
-    timeSignature: "4/4",
-    editor: "烏鴉Lin",
-    content: "[A]但[C#m]願 尊[D]榮歸[Bm]於主我[A]神 祂作[D]為完全所行\n[E]盡[E7]都公[A]義[C#m] [D]但願 尊[Bm]榮歸於主我神 祂\n[A]作為[D]完全所行[E]盡[E7]都公[A]義[C#m] 信實 全能的真\n[D]神 公[E]義聖[Bm]潔之[E]主 [A]祂是[E7]正直良[A]善 信實\n[C#m]全能的真[D]神 公[E]義聖[Bm]潔之[E]主 祂是正直良\n[A]善[D][A]"
+    content: "[D]我主耶穌 [A/C#]已被高舉 [G]你配得榮耀 [A]高升在天上\n[D]他已賜下 [A/C#]他的聖靈 [G]加添我力量 [Em]是隨時的幫[A]助\n[G]我何等渴[D/F#]慕 能一[Em]生與你同[A]行 [D]到永[D7]遠\n(副)親愛[G]主聖靈[A/G] 觸摸[F#m]我的心[Bm]\n恩[Em]膏澆[A]灌我[Am]靈 [D7]\n主我[G]渴想你[A/G] 更加[F#m]貼近你[Bm]\n住[Em]在你[A]同在[D]裡"
   }
 ];
