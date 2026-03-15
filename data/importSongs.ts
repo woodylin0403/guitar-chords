810fb716d93da67c25a132273f7cc3a45b1fe1d9
woodylin0403 <coolcrow0403@gmail.com>
2026-03-15 08:24:13 +0800

    newsong2

--- data/importSongs.ts
+++ data/importSongs.ts
@@ -1,9 +1,10 @@
 export const importSongs = [
   {
-    title: "53. 親愛的救主",
-    originalKey: "G",
+    title: "主的喜樂是我力量 (讚美之泉)",
+    originalKey: "E",
     timeSignature: "4/4",
     editor: "烏鴉Lin",
-    content: "[G]親愛的救主 [Am7]我願更深親[G/B]近你 [C]你與[Am]我\n[A]同行擔我苦難 [D7]你大愛何等完[G]全 [B7]永不\n[Em]改[G/D]變 [C]我一生單單[D7]要來[C/G]服事[G]你"
+    youtubeUrl: "https://www.youtube.com/watch?v=eTEaZ6WA2dI",
+    content: "[|] [E]主的喜樂是 [|] [A]我力量 [|] [E]你的救恩是 [|] [B]我盼望 [|]\n[|] [A]雖然橄欖樹 [|] [E/G#]不效力 [|] [F#m]也許葡萄樹 [|] [B]不結果 [|]\n[|] [F#m]我仍因 [|] [B]救我的神歡欣快 [|] [E]樂 [|] [E] [|]\n\n(副歌)\n[|] [E]我要讚美 無論 [|] [A]得時或不 [B]得時 [|]\n[|] [E]我要讚美 每天從 [|] [A]日出到日 [B]落 [|]\n[|] [F#m]我心堅 [B]定於你 [|] [G#m]每天讚美不 [C#m]停 [|]\n[|] [A]我要讚美 [E/G#]跳舞讚美 [|] [A]我要讚美 [E/G#]自由讚美 [|]\n[|] [F#m]大聲歡呼 [B]你是永遠得勝君 [|] [E]王 [|] [E] [|]"
   }
 ];
