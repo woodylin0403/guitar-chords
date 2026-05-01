9f13a26eda6597aaa3c505c20bd61543310ee884
woodylin0403 <coolcrow0403@gmail.com>
2026-05-01 14:11:17 +0800

    投票7

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // 🌟 就是這行！沒有它就絕對生不出 out
+  images: { unoptimized: true } 
 };
-
 export default nextConfig;
