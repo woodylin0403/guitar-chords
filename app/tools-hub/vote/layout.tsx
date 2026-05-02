799fa6df8cbf4df16df170e3c6c6fa0e9227ff6e
woodylin0403 <coolcrow0403@gmail.com>
2026-05-02 11:10:32 +0800

    網址

--- app/tools-hub/vote/layout.tsx
+++ app/tools-hub/vote/layout.tsx
@@ -1,10 +1,17 @@
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: '神學學院戲劇決選',
-  description: '數位聖殿 - 即時戲劇決選投票系統',
+  title: '數位聖殿｜神學學院戲劇決選',
+  description: '神聖通道已開啟！請點擊進入數位聖殿，為您心中的最佳啟示投下神聖的一票。',
   icons: {
-    icon: '🏆', // 這裡可以換成你喜歡的 Emoji，或是你自己的 favicon 網址
+    icon: '🏆',
+  },
+  // 🌟 加入 Open Graph 專屬設定 (專門給 LINE, Facebook, IG 預覽用的)
+  openGraph: {
+    title: '數位聖殿｜神學學院戲劇決選',
+    description: '神聖通道已開啟！請點擊進入數位聖殿，為您心中的最佳啟示投下神聖的一票。',
+    siteName: '數位聖殿投票系統',
+    type: 'website',
   },
 };
 
